12e6710a03fa75b8427e49921b64bf808b482e99
redvelvet511 <113856982+redvelvet511@users.noreply.github.com>
2023-06-16 12:38:20 -0400

    SVG take up content-container height

--- express/blocks/hero-color/hero-color.js
+++ express/blocks/hero-color/hero-color.js
@@ -51,23 +51,22 @@ function cloneForSmallerMediaQueries(textBlock) {
 
 function changeTextColorAccordingToBg(
   primaryColor,
-  heroColorContentContainer,
+  block,
 ) {
-  heroColorContentContainer.classList.add(isDarkOverlayReadable(primaryColor) ? 'light-bg' : 'dark-bg');
+  block.classList.add(isDarkOverlayReadable(primaryColor) ? 'light-bg' : 'dark-bg');
 }
 
-function loadSvgInsideWrapper(mediaQuery, svgId, svgWrapper, secondaryColor) {
-  const size = mediaQuery.matches ? 'desktop' : 'mobile';
+function loadSvgInsideWrapper(svgId, svgWrapper, secondaryColor) {
   const svgNS = 'http://www.w3.org/2000/svg';
   const xlinkNS = 'http://www.w3.org/1999/xlink';
 
   // create svg element
   const svg = document.createElementNS(svgNS, 'svg');
-  svg.setAttribute('class', 'color-svg-img');
+  svg.setAttribute('class', 'color-svg-img hidden-svg');
 
   // create use element
   const useSvg = document.createElementNS(svgNS, 'use');
-  useSvg.setAttributeNS(xlinkNS, 'xlink:href', `/express/icons/color-sprite.svg#${svgId}-${size}`);
+  useSvg.setAttributeNS(xlinkNS, 'xlink:href', `/express/icons/color-sprite.svg#${svgId}`);
 
   // append use element to svg element
   svg.appendChild(useSvg);
@@ -78,21 +77,13 @@ function loadSvgInsideWrapper(mediaQuery, svgId, svgWrapper, secondaryColor) {
   svgWrapper.firstElementChild.style.fill = secondaryColor;
 }
 
-function changeSvgAccordingToMediaQuery(svgId, svgWrapper, secondaryColor) {
-  const mediaQuery = window.matchMedia('(min-width: 900px)');
-  loadSvgInsideWrapper(mediaQuery, svgId, svgWrapper, secondaryColor);
-  mediaQuery.addEventListener('change', (event) => {
-    loadSvgInsideWrapper(event, svgId, svgWrapper, secondaryColor);
-  });
-}
-
 function displaySvgWithObject(block, secondaryColor) {
   const svg = block.firstElementChild;
   const svgId = svg.firstElementChild.textContent;
   const svgWrapper = createTag('div', { class: 'color-svg' });
 
   svg.remove();
-  changeSvgAccordingToMediaQuery(svgId, svgWrapper, secondaryColor);
+  loadSvgInsideWrapper(svgId, svgWrapper, secondaryColor);
   const heroColorContentContainer = block.querySelector('.content-container');
   heroColorContentContainer.append(svgWrapper);
 }
@@ -119,9 +110,10 @@ function extractColorElements(colors) {
 function decorateColors(block) {
   const colors = block.firstElementChild;
   const { primaryColor, secondaryColor } = extractColorElements(colors);
-  const heroColorContentContainer = block.querySelector('.content-container');
-  heroColorContentContainer.style.backgroundColor = primaryColor;
-  changeTextColorAccordingToBg(primaryColor, heroColorContentContainer);
+
+  block.style.backgroundColor = primaryColor;
+
+  changeTextColorAccordingToBg(primaryColor, block);
 
   return { secondaryColor };
 }
@@ -136,10 +128,44 @@ function decorateText(block) {
   block.append(smallMediaQueryBlock);
 }
 
+function getContentContainerHeight() {
+  const contentContainer = document.querySelector('.content-container');
+
+  return contentContainer?.clientHeight;
+}
+
+function resizeSvgOnLoad() {
+  const interval = setInterval(() => {
+    if (document.readyState === 'complete') {
+      const height = getContentContainerHeight();
+      if (height) {
+        const svg = document.querySelector('.color-svg-img');
+        svg.classList.remove('hidden-svg');
+        svg.style.height = `${height}px`;
+        clearInterval(interval);
+      }
+    }
+  }, 50);
+}
+
+function resizeSvgOnMediaQueryChange() {
+  const mediaQuery = window.matchMedia('(min-width: 900px)');
+  mediaQuery.addEventListener('change', (event) => {
+    const height = getContentContainerHeight();
+    const svg = document.querySelector('.color-svg-img');
+    if (event.matches) {
+      svg.style.height = `${height}px`;
+    } else {
+      svg.style.height = '200px';
+    }
+  });
+}
+
 export default function decorate(block) {
   const heroColorContentContainer = createTag('div', {
     class: 'content-container',
   });
+
   block.append(heroColorContentContainer);
 
   // text
@@ -150,4 +176,6 @@ export default function decorate(block) {
 
   // svg
   displaySvgWithObject(block, secondaryColor);
+  resizeSvgOnLoad();
+  resizeSvgOnMediaQueryChange();
 }
