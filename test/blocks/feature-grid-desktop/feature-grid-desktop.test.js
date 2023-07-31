04ec6550f9397f6045bf02b462b04debfe9209ae
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-07-31 15:48:18 -0400

    Second test

--- test/blocks/feature-grid-desktop/feature-grid-desktop.test.js
+++ test/blocks/feature-grid-desktop/feature-grid-desktop.test.js
@@ -9,23 +9,45 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
 
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
+// import {
+//   decorate
+//   // renderImageOrVideo,
+//   // renderGridNode,
+//   // decorateLoadMoreButton,
+//   // getGradient,
+// } from '../../../express/blocks/feature-grid-desktop/feature-grid-desktop.js';
+
+const { default: decorate } = await import('../../../express/blocks/feature-grid-desktop/feature-grid-desktop.js');
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
-const { default: decorate } = await import('../../../express/blocks/feature-grid-desktop/feature-grid-desktop.js');
-// What does the default do?
 
-// Describe method takes two args: description and a function to do testing
 describe('Feature Grid Desktop', () => {
   before(() => {
     window.isTestEnv = true;
   });
+  const featureGrid = document.querySelector('.feature-grid-desktop');
+  decorate(featureGrid);
+  const cellList = featureGrid.querySelectorAll('.grid-item');
 
   it('check if Feature Grid block exists', () => {
-    const featureGrid = document.querySelector('.feature-grid-desktop');
-    decorate(featureGrid);
     expect(featureGrid).to.exist;
   });
+
+  cellList.forEach((cell) => {
+    it('each cell has all expected elements', () => {
+      expect(cell.querySelector('h2')).to.exist;
+      expect(cell.querySelector('p')).to.exist;
+      expect(cell.querySelector('.cta')).to.exist;
+      expect(cell.querySelector('img, video')).to.exist;
+    });
+  });
+
+  // it('returns null if video href is empty', () => {
+  //   expect(renderImageOrVideo('<a href=""></a>')).to.equal(null);
+  // });
 });
