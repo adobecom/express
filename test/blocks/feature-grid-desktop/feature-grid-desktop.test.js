a1104f02b8acd5c2db18d955ffe8364312426ace
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-08-04 10:51:31 -0400

    Refactored into separate describe functions

--- test/blocks/feature-grid-desktop/feature-grid-desktop.test.js
+++ test/blocks/feature-grid-desktop/feature-grid-desktop.test.js
@@ -19,65 +19,100 @@ const { default: decorate } = await import('../../../express/blocks/feature-grid
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 
 describe('Feature Grid Desktop', async () => {
-  const smallGrid = document.querySelector('#small-grid');
-  const oversizedGrid = document.querySelector('#over-sized-grid');
-  const noGradientBlock = document.querySelector('#no-gradient');
-  // console.log(noGradientBlock);
-  const cellList = document.querySelectorAll('#full-grid .grid-item');
-  let loadMoreButton;
-  let fullGrid;
   before(() => {
     window.isTestEnv = true;
+  });
+
+  describe('Small Grid', () => {
+    const smallGrid = document.querySelector('#small-grid');
     decorate(smallGrid);
-    decorate(noGradientBlock);
-  });
 
-  beforeEach(async () => {
-    document.body.innerHTML = await readFile({ path: './mocks/body.html' });
-    fullGrid = document.querySelector('#full-grid');
-    decorate(fullGrid);
-    loadMoreButton = document.querySelector('#full-grid .load-more-button');
-  });
+    it('has a heading', () => {
+      const heading = smallGrid.querySelector('.heading > h1');
+      expect(heading).to.exist;
+    });
 
-  it('check if Feature Grid block exists', () => {
-    expect(smallGrid).to.exist;
-  });
+    it('has subtext in the heading', () => {
+      const subtext = smallGrid.querySelector('.heading > p');
+      expect(subtext).to.exist;
+    });
 
-  cellList.forEach((cell) => {
-    it('each cell has all expected elements', () => {
-      expect(cell.querySelector('h2')).to.exist;
-      expect(cell.querySelector('p')).to.exist;
-      expect(cell.querySelector('.cta')).to.exist;
-      expect(cell.querySelector('img, video')).to.exist;
+    it('has all correct elements in each cell', () => {
+      const cellList = smallGrid.querySelectorAll('.grid-item');
+      cellList.forEach((cell) => {
+        expect(cell.querySelector('h2')).to.exist;
+        expect(cell.querySelector('p')).to.exist;
+        expect(cell.querySelector('.cta')).to.exist;
+        expect(cell.querySelector('img, video')).to.exist;
+      });
     });
   });
 
-  it('gives an error message if too many cells are passed in', () => {
-    expect(() => decorate(oversizedGrid)).to.throw('Authoring issue: Feature Grid Fixed block should have 12 children. Received: 14');
+  describe('Full Grid', () => {
+    let fullGrid;
+    let loadMoreButton;
+    beforeEach(async () => {
+      document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+      fullGrid = document.querySelector('#full-grid');
+      decorate(fullGrid);
+      loadMoreButton = fullGrid.querySelector('.load-more-button');
+    });
+
+    it('has a heading', () => {
+      const heading = fullGrid.querySelector('.heading > h1');
+      expect(heading).to.exist;
+    });
+
+    it('has subtext in the heading', () => {
+      const subtext = fullGrid.querySelector('.heading > p');
+      expect(subtext).to.exist;
+    });
+
+    it('has all correct elements in each cell', () => {
+      const cellList = document.querySelectorAll('#full-grid .grid-item');
+      cellList.forEach((cell) => {
+        expect(cell.querySelector('h2')).to.exist;
+        expect(cell.querySelector('p')).to.exist;
+        expect(cell.querySelector('.cta')).to.exist;
+        expect(cell.querySelector('img, video')).to.exist;
+      });
+    });
+
+    it('adds the expanded class to the block when "Load More" is clicked', () => {
+      loadMoreButton.click();
+      expect(Object.values(fullGrid.classList)).to.include('expanded');
+    });
+
+    it('adds the authored color back to the background gradient of the "load-more" section when toggled off/on again', async () => {
+      const loadMoreDiv = fullGrid.querySelector('.load-more-div');
+      loadMoreButton.click();
+      loadMoreButton.click();
+      expect(loadMoreDiv.style.background).to.equal('linear-gradient(rgba(255, 255, 255, 0), rgb(252, 250, 255), rgb(252, 250, 255))');
+    });
+
+    it('has the correct text in the "Load more" button', () => {
+      expect(loadMoreButton.textContent).to.equal('Explore more Adobe Express features');
+      loadMoreButton.click();
+      expect(loadMoreButton.textContent).to.equal('View less');
+    });
   });
 
-  it('adds the expanded class to the block when "Load More" is clicked', () => {
-    loadMoreButton.click();
-    expect(Object.values(fullGrid.classList)).to.include('expanded');
+  describe('Oversized Grid', () => {
+    const oversizedGrid = document.querySelector('#over-sized-grid');
+    it('gives an error message if too many cells are passed in', () => {
+      expect(() => decorate(oversizedGrid)).to.throw('Authoring issue: Feature Grid Fixed block should have 12 children. Received: 14');
+    });
   });
 
-  it('adds the authored color to the background gradient of the "load-more" section when clicked', async () => {
-    const loadMoreDiv = fullGrid.querySelector('.load-more-div');
-    loadMoreButton.click();
-    loadMoreButton.click();
-    expect(loadMoreDiv.style.background).to.equal('linear-gradient(rgba(255, 255, 255, 0), rgb(252, 250, 255), rgb(252, 250, 255))');
-  });
-
-  it('leaves gradient blank if none authored', () => {
-    const loadMoreDiv = noGradientBlock.querySelector('.load-more-div');
-    loadMoreButton.click();
-    loadMoreButton.click();
-    expect(loadMoreDiv.style.background).to.equal('');
-  });
-
-  it('Allows author to control text in the "load more" button', () => {
-    expect(loadMoreButton.textContent).to.equal('Explore more Adobe Express features');
-    loadMoreButton.click();
-    expect(loadMoreButton.textContent).to.equal('View less');
+  describe('Unauthored gradient', () => {
+    it('leaves gradient blank if none authored (styled by CSS)', () => {
+      const noGradientGrid = document.querySelector('#no-gradient');
+      decorate(noGradientGrid);
+      const loadMoreButton = noGradientGrid.querySelector('.load-more-button');
+      const loadMoreDiv = noGradientGrid.querySelector('.load-more-div');
+      loadMoreButton.click();
+      loadMoreButton.click();
+      expect(loadMoreDiv.style.background).to.equal('');
+    });
   });
 });
