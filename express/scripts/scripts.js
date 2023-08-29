c6310729449e6962f0faba605e06c11af4e4d5ac
Qiyun Dai <pianoquin@gmail.com>
2023-08-29 11:06:35 -0500

    remove comments

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -1544,12 +1544,6 @@ function unwrapBlock($block) {
   if (!$postBlockSection.hasChildNodes()) {
     $postBlockSection.remove();
   }
-  //
-  // if (!$section.hasChildNodes()) {
-  //   $section.remove();
-  // }
-  // fixme: technically $section can become empty too after unwrapping.
-  //  This function currently leaves empty section after the generation of relevant rows
 }
 
 export function normalizeHeadings(block, allowedHeadings) {
