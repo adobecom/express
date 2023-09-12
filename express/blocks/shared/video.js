166df3c332c5b474c349c72d1a4234cb7f3a222b
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-09-12 10:46:34 -0400

    Adjusted no-scroll for modal

--- express/blocks/shared/video.js
+++ express/blocks/shared/video.js
@@ -192,16 +192,16 @@ export function hideVideoModal(push) {
     window.history.pushState({}, docTitle, window.location.href.split('#')[0]);
   }
 
-  const scrollY = document.body.style.top;
-  console.log('Close Modal, scroll to:', scrollY);
+  // const scrollY = document.body.style.top;
+  // console.log('Close Modal, scroll to:', scrollY);
   body.classList.remove('no-scroll');
-  window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+  // window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
 }
 
 export function displayVideoModal(url = [], title, push) {
   const body = document.querySelector('body');
-  console.log('Open Modal, scroll to:', window.scrollY);
-  body.style.top = `${-window.scrollY}px`;
+  // console.log('Open Modal, scroll to:', window.scrollY);
+  // body.style.top = `${-window.scrollY}px`;
   body.classList.add('no-scroll');
 
   let vidUrls = typeof url === 'string' ? [url] : url;
