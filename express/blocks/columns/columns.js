65e6723f4eff8e0bb9ca55a98a4529a49c0bb8f3
Qiyun Dai <pianoquin@gmail.com>
2023-09-19 21:06:34 -0500

    dollar sign removal

--- express/blocks/columns/columns.js
+++ express/blocks/columns/columns.js
@@ -28,36 +28,37 @@ import {
   isVideoLink,
 } from '../shared/video.js';
 
-function transformToVideoColumn($cell, $a, block) {
-  const $parent = $cell.parentElement;
-  const title = $a.textContent.trim();
+function transformToVideoColumn(cell, aTag, block) {
+  const parent = cell.parentElement;
+  const title = aTag.textContent.trim();
   // gather video urls from all links in cell
   const vidUrls = [];
-  $cell.querySelectorAll(':scope a.button').forEach(($button) => {
-    vidUrls.push($button.href);
-    if ($button !== $a) {
-      $button.closest('.button-container').remove();
+  cell.querySelectorAll(':scope a.button').forEach((button) => {
+    vidUrls.push(button.href);
+    if (button !== aTag) {
+      button.closest('.button-container').remove();
     }
   });
-  $a.setAttribute('rel', 'nofollow');
+  aTag.setAttribute('rel', 'nofollow');
 
-  $cell.classList.add('column-video');
-  $parent.classList.add('columns-video');
+  cell.classList.add('column-video');
+  parent.classList.add('columns-video');
 
   setTimeout(() => {
-    const $sibling = $parent.querySelector('.column-picture');
-    if ($sibling && block.classList.contains('highlight')) {
-      const $videoOverlay = createTag('div', { class: 'column-video-overlay' });
-      const $videoOverlayIcon = getIconElement('play', 44);
-      $videoOverlay.append($videoOverlayIcon);
-      $sibling.append($videoOverlay);
+    const sibling = parent.querySelector('.column-picture');
+    if (sibling && block.classList.contains('highlight')) {
+      const videoOverlay = createTag('div', { class: 'column-video-overlay' });
+      const videoOverlayIcon = getIconElement('play', 44);
+      videoOverlay.append(videoOverlayIcon);
+      sibling.append(videoOverlay);
     }
   }, 1);
 
-  const modalActivator = block.classList.contains('highlight') ? $parent : $a;
+  const modalActivator = block.classList.contains('highlight') ? parent : aTag;
   modalActivator.addEventListener('click', () => {
     displayVideoModal(vidUrls, title, true);
   });
+
   modalActivator.addEventListener('keyup', ({ key }) => {
     if (key === 'Enter') {
       displayVideoModal(vidUrls, title);
@@ -70,17 +71,17 @@ function transformToVideoColumn($cell, $a, block) {
   }
 }
 
-function decorateIconList($columnCell, rowNum, blockClasses) {
-  const icons = [...$columnCell.querySelectorAll('img.icon, svg.icon')]
-    .filter(($icon) => !$icon.closest('p').classList.contains('social-links'));
+function decorateIconList(columnCell, rowNum, blockClasses) {
+  const icons = [...columnCell.querySelectorAll('img.icon, svg.icon')]
+    .filter((icon) => !icon.closest('p').classList.contains('social-links'));
   // decorate offer icons
   if (rowNum === 0 && blockClasses.contains('offer')) {
-    const $titleIcon = $columnCell.querySelector('img.icon, svg.icon');
-    const $title = $columnCell.querySelector('h1, h2, h3, h4, h5, h6');
-    if ($title && $titleIcon) {
-      const $titleIconWrapper = createTag('span', { class: 'columns-offer-icon' });
-      $titleIconWrapper.append($titleIcon);
-      $title.prepend($titleIconWrapper);
+    const titleIcon = columnCell.querySelector('img.icon, svg.icon');
+    const title = columnCell.querySelector('h1, h2, h3, h4, h5, h6');
+    if (title && titleIcon) {
+      const titleIconWrapper = createTag('span', { class: 'columns-offer-icon' });
+      titleIconWrapper.append(titleIcon);
+      title.prepend(titleIconWrapper);
     }
     return;
   }
@@ -91,35 +92,35 @@ function decorateIconList($columnCell, rowNum, blockClasses) {
     && !icons[0].closest('p').previousElementSibling) {
     // treat icon as brand icon if first element in first row cell and no text next to it
     icons[0].classList.add('brand');
-    $columnCell.parentElement.classList.add('has-brand');
+    columnCell.parentElement.classList.add('has-brand');
     return;
   }
   if (icons.length) {
-    let $iconList = createTag('div', { class: 'columns-iconlist' });
-    let $iconListDescription;
-    [...$columnCell.children].forEach(($e) => {
+    let iconList = createTag('div', { class: 'columns-iconlist' });
+    let iconListDescription;
+    [...columnCell.children].forEach(($e) => {
       const imgs = $e.querySelectorAll('img.icon, svg.icon');
       // only build icon list if single icon plus text
-      const $img = imgs.length === 1 ? imgs[0] : null;
-      const hasText = $img ? $img.closest('p').textContent.trim() !== '' : false;
-      if ($img && hasText) {
-        const $iconListRow = createTag('div');
-        const $iconDiv = createTag('div', { class: 'columns-iconlist-icon' });
-        $iconDiv.appendChild($img);
-        $iconListRow.append($iconDiv);
-        $iconListDescription = createTag('div', { class: 'columns-iconlist-description' });
-        $iconListRow.append($iconListDescription);
-        $iconListDescription.appendChild($e);
-        $iconList.appendChild($iconListRow);
+      const img = imgs.length === 1 ? imgs[0] : null;
+      const hasText = img ? img.closest('p').textContent.trim() !== '' : false;
+      if (img && hasText) {
+        const iconListRow = createTag('div');
+        const iconDiv = createTag('div', { class: 'columns-iconlist-icon' });
+        iconDiv.appendChild(img);
+        iconListRow.append(iconDiv);
+        iconListDescription = createTag('div', { class: 'columns-iconlist-description' });
+        iconListRow.append(iconListDescription);
+        iconListDescription.appendChild($e);
+        iconList.appendChild(iconListRow);
       } else {
-        if ($iconList.children.length > 0) {
-          $columnCell.appendChild($iconList);
-          $iconList = createTag('div', { class: 'columns-iconlist' });
+        if (iconList.children.length > 0) {
+          columnCell.appendChild(iconList);
+          iconList = createTag('div', { class: 'columns-iconlist' });
         }
-        $columnCell.appendChild($e);
+        columnCell.appendChild($e);
       }
     });
-    if ($iconList.children.length > 0) $columnCell.appendChild($iconList);
+    if (iconList.children.length > 0) columnCell.appendChild(iconList);
   }
 }
 
@@ -138,30 +139,30 @@ const handleVideos = (cell, a, block, thumbnail) => {
   }
 };
 
-export default function decorate($block) {
-  const $rows = Array.from($block.children);
+export default function decorate(block) {
+  const rows = Array.from(block.children);
 
   let numCols = 0;
-  if ($rows[0]) numCols = $rows[0].children.length;
+  if (rows[0]) numCols = rows[0].children.length;
 
-  if (numCols) $block.classList.add(`width-${numCols}-columns`);
+  if (numCols) block.classList.add(`width-${numCols}-columns`);
 
-  let total = $rows.length;
-  const isNumberedList = $block.classList.contains('numbered');
-  if (isNumberedList && $block.classList.length > 4) {
-    const i = parseInt($block.classList[3], 10);
+  let total = rows.length;
+  const isNumberedList = block.classList.contains('numbered');
+  if (isNumberedList && block.classList.length > 4) {
+    const i = parseInt(block.classList[3], 10);
     // eslint-disable-next-line no-restricted-globals
     if (!isNaN(i)) {
       total = i;
     }
   }
 
-  $rows.forEach(($row, rowNum) => {
-    const $cells = Array.from($row.children);
+  rows.forEach((row, rowNum) => {
+    const cells = Array.from(row.children);
 
-    $cells.forEach(($cell, cellNum) => {
-      const $a = $cell.querySelector('a');
-      const $pics = $cell.querySelectorAll(':scope picture');
+    cells.forEach((cell, cellNum) => {
+      const aTag = cell.querySelector('a');
+      const pics = cell.querySelectorAll(':scope picture');
 
       if (cellNum === 0 && isNumberedList) {
         // add number to first cell
@@ -177,37 +178,37 @@ export default function decorate($block) {
           // regular ordered list style for 1 to 9 items
           num = `${num}.`;
         }
-        $cell.innerHTML = `<span class="num">${num}</span>${$cell.innerHTML}`;
+        cell.innerHTML = `<span class="num">${num}</span>${cell.innerHTML}`;
       }
 
-      if ($pics.length === 1 && $pics[0].parentElement.tagName === 'P') {
+      if (pics.length === 1 && pics[0].parentElement.tagName === 'P') {
         // unwrap single picture if wrapped in p tag, see https://github.com/adobe/helix-word2md/issues/662
-        const $parentDiv = $pics[0].closest('div');
-        const $parentParagraph = $pics[0].parentNode;
-        $parentDiv.insertBefore($pics[0], $parentParagraph);
+        const parentDiv = pics[0].closest('div');
+        const parentParagraph = pics[0].parentNode;
+        parentDiv.insertBefore(pics[0], parentParagraph);
       }
 
-      if ($cell.querySelector('img.icon, svg.icon')) {
-        decorateIconList($cell, rowNum, $block.classList);
+      if (cell.querySelector('img.icon, svg.icon')) {
+        decorateIconList(cell, rowNum, block.classList);
       }
-      if (isVideoLink($a?.href)) {
-        handleVideos($cell, $a, $block, $pics[0]);
+      if (isVideoLink(aTag?.href)) {
+        handleVideos(cell, aTag, block, pics[0]);
       }
 
-      if ($a?.textContent.trim().startsWith('https://')) {
-        if ($a.href.endsWith('.mp4')) {
-          transformLinkToAnimation($a);
-        } else if ($pics[0]) {
-          linkImage($cell);
+      if (aTag?.textContent.trim().startsWith('https://')) {
+        if (aTag.href.endsWith('.mp4')) {
+          transformLinkToAnimation(aTag);
+        } else if (pics[0]) {
+          linkImage(cell);
         }
       }
 
-      if ($a && $a.classList.contains('button')) {
-        if ($block.className.includes('fullsize')) {
-          $a.classList.add('xlarge');
-          $a.classList.add('primaryCTA');
-        } else if ($a.classList.contains('light')) {
-          $a.classList.replace('accent', 'primary');
+      if (aTag && aTag.classList.contains('button')) {
+        if (block.className.includes('fullsize')) {
+          aTag.classList.add('xlarge');
+          aTag.classList.add('primaryCTA');
+        } else if (aTag.classList.contains('light')) {
+          aTag.classList.replace('accent', 'primary');
         }
       }
 
@@ -220,20 +221,20 @@ export default function decorate($block) {
         }
       });
 
-      $cell.querySelectorAll(':scope p:empty').forEach(($p) => {
+      cell.querySelectorAll(':scope p:empty').forEach(($p) => {
         if ($p.innerHTML.trim() === '') {
           $p.remove();
         }
       });
 
-      $cell.classList.add('column');
-      const childEls = [...$cell.children];
+      cell.classList.add('column');
+      const childEls = [...cell.children];
       const isPictureColumn = childEls.every((el) => ['BR', 'PICTURE'].includes(el.tagName)) && childEls.length > 0;
       if (isPictureColumn) {
-        $cell.classList.add('column-picture');
+        cell.classList.add('column-picture');
       }
 
-      const $pars = $cell.querySelectorAll('p');
+      const $pars = cell.querySelectorAll('p');
       for (let i = 0; i < $pars.length; i += 1) {
         if ($pars[i].innerText.match(/Powered by/)) {
           $pars[i].classList.add('powered-by');
@@ -241,41 +242,41 @@ export default function decorate($block) {
       }
     });
   });
-  addAnimationToggle($block);
-  addHeaderSizing($block, 'columns-heading');
+  addAnimationToggle(block);
+  addHeaderSizing(block, 'columns-heading');
 
   // decorate offer
-  if ($block.classList.contains('offer')) {
-    $block.querySelectorAll('a.button').forEach(($a) => $a.classList.add('large', 'wide'));
-    if ($rows.length > 1) {
+  if (block.classList.contains('offer')) {
+    block.querySelectorAll('a.button').forEach((aTag) => aTag.classList.add('large', 'wide'));
+    if (rows.length > 1) {
       // move all content into first row
-      $rows.forEach(($row, rowNum) => {
+      rows.forEach((row, rowNum) => {
         if (rowNum > 0) {
-          const $cells = Array.from($row.children);
-          $cells.forEach(($cell, cellNum) => {
-            $rows[0].children[cellNum].append(...$cell.children);
+          const cells = Array.from(row.children);
+          cells.forEach((cell, cellNum) => {
+            rows[0].children[cellNum].append(...cell.children);
           });
-          $row.remove();
+          row.remove();
         }
       });
     }
   }
 
   // add free plan widget to first columns block on every page
-  if (document.querySelector('main .columns') === $block
-    && document.querySelector('main .block') === $block) {
-    addFreePlanWidget($block.querySelector('.button-container')
-      || $block.querySelector(':scope .column:not(.hero-animation-overlay,.columns-picture)'));
+  if (document.querySelector('main .columns') === block
+    && document.querySelector('main .block') === block) {
+    addFreePlanWidget(block.querySelector('.button-container')
+      || block.querySelector(':scope .column:not(.hero-animation-overlay,.columns-picture)'));
   }
 
   // invert buttons in regular columns inside columns-highlight-container
-  if ($block.closest('.section.columns-highlight-container') && !$block.classList.contains('highlight')) {
-    $block.querySelectorAll('a.button').forEach(($button) => {
-      $button.classList.add('dark');
+  if (block.closest('.section.columns-highlight-container') && !block.classList.contains('highlight')) {
+    block.querySelectorAll('a.button').forEach((button) => {
+      button.classList.add('dark');
     });
   }
 
-  if ($block.className === 'columns fullsize top block width-3-columns') {
+  if (block.className === 'columns fullsize top block width-3-columns') {
     const setElementsHeight = (columns) => {
       const elementsMinHeight = {
         PICTURE: 0,
@@ -321,31 +322,31 @@ export default function decorate($block) {
               });
             });
 
-            observer.unobserve($block);
+            observer.unobserve(block);
           }
         });
       };
 
       const observer = new IntersectionObserver(onIntersect, { threshold: 0 });
-      observer.observe($block);
+      observer.observe(block);
     };
 
-    setElementsHeight($block.querySelectorAll('.column'));
+    setElementsHeight(block.querySelectorAll('.column'));
   }
 
   // variant for the colors pages
-  if ($block.classList.contains('color')) {
-    const [primaryColor, accentColor] = $rows[1].querySelector('div').textContent.trim().split(',');
-    const [textCol, svgCol] = Array.from(($rows[0].querySelectorAll('div')));
+  if (block.classList.contains('color')) {
+    const [primaryColor, accentColor] = rows[1].querySelector('div').textContent.trim().split(',');
+    const [textCol, svgCol] = Array.from((rows[0].querySelectorAll('div')));
     const svgId = svgCol.textContent.trim();
     const svg = createTag('div', { class: 'img-wrapper' });
 
     svgCol.remove();
-    $rows[1].remove();
+    rows[1].remove();
     textCol.classList.add('text');
     svg.innerHTML = `<svg class='color-svg-img'> <use href='/express/icons/color-sprite.svg#${svgId}'></use></svg>'`;
     svg.style.backgroundColor = primaryColor;
     svg.style.fill = accentColor;
-    $rows[0].append(svg);
+    rows[0].append(svg);
   }
 }
