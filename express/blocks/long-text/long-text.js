3687ce75260deb653f52381ee947a6e7d4f23a7e
Qiyun Dai <pianoquin@gmail.com>
2023-09-27 12:31:57 -0500

    revert long-text block changes to avoid conflict

--- express/blocks/long-text/long-text.js
+++ express/blocks/long-text/long-text.js
@@ -11,10 +11,6 @@
  */
 
 export default function decorate(block) {
-  if (block.classList.contains('plain')) {
-    block.parentElement.classList.add('plain');
-  }
-
   if (block.textContent.trim() === '') {
     if (block.parentElement.classList.contains('long-text-wrapper')) {
       block.parentElement.remove();
