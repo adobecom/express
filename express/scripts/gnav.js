7060ac37c1eafd569578162a81ac43f5962edebf
Jingle Huang <32369333+JingleH@users.noreply.github.com>
2023-10-02 16:11:59 -0700

    use legal terms (#325)
    Co-authored-by: Jingle Huang <jinglhua@adobe.com>

--- express/scripts/gnav.js
+++ express/scripts/gnav.js
@@ -82,7 +82,7 @@ async function loadFEDS() {
     const $body = document.body;
     const regionpath = locale === 'us' ? '/' : `/${locale}/`;
     const host = window.location.hostname === 'localhost' ? 'https://www.adobe.com' : '';
-    const url = locale === 'us' ? `${host}${regionpath}creativecloud.html` : `${host}${regionpath}`;
+    const url = locale === 'us' ? `${host}${regionpath}legal/terms.html` : `${host}${regionpath}`;
     const resp = await fetch(url);
     const html = await resp.text();
     const $div = createTag('div');
