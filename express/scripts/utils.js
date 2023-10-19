ecfe5229a5db9f4170349e277ce5c5364475656b
Jingle Huang <jinglhua@adobe.com>
2023-10-18 21:17:31 -0500

    gnav CSO

--- express/scripts/utils.js
+++ express/scripts/utils.js
@@ -2468,7 +2468,7 @@ export async function loadArea(area = document) {
   }
 
   if (blog) await loadAndExecute('/express/styles/blog.css', '/express/scripts/blog.js');
-  loadBlocks(sections);
+  loadBlocks(sections, true);
   const footer = document.querySelector('footer');
   delete footer.dataset.status;
 
