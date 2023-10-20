f34243cc14b29efdeedb4131701bb19f665b8158
Qiyun Dai <pianoquin@gmail.com>
2023-10-20 17:07:44 -0500

    test fragment fade-in

--- express/blocks/fragment/fragment.js
+++ express/blocks/fragment/fragment.js
@@ -41,30 +41,43 @@ async function loadFragment(path) {
   return null;
 }
 
+function injectFragmentOnPage(block, fragment) {
+  const fragmentSection = fragment.querySelector(':scope .section');
+  const blockSection = block.closest('.section');
+
+  if (fragmentSection) {
+    const audience = fragmentSection.dataset?.audience;
+    if (audience) {
+      if (audience !== document.body.dataset?.device) {
+        block.closest('.section').remove();
+        return;
+      }
+      block.closest('.section').dataset.audience = audience;
+    }
+
+    block.closest('.section')?.classList.add(...fragmentSection.classList);
+    if (block.closest('.fragment-wrapper')) {
+      block.closest('.fragment-wrapper').replaceWith(...fragmentSection.childNodes);
+    } else {
+      block.replaceWith(...fragmentSection.childNodes);
+    }
+
+    if (blockSection && blockSection.dataset.toggle) {
+      block.style.opacity = '0';
+      block.style.transition = 'opacity 0.5s';
+      setTimeout(() => {
+        block.style.opacity = '1';
+      });
+    }
+  }
+}
+
 export default async function decorate(block) {
   const link = block.querySelector('a');
   const path = link ? link.getAttribute('href') : block.textContent.trim();
   const fragment = await loadFragment(path);
 
   if (fragment) {
-    const fragmentSection = fragment.querySelector(':scope .section');
-
-    if (fragmentSection) {
-      const audience = fragmentSection.dataset?.audience;
-      if (audience) {
-        if (audience !== document.body.dataset?.device) {
-          block.closest('.section').remove();
-          return;
-        }
-        block.closest('.section').dataset.audience = audience;
-      }
-
-      block.closest('.section')?.classList.add(...fragmentSection.classList);
-      if (block.closest('.fragment-wrapper')) {
-        block.closest('.fragment-wrapper').replaceWith(...fragmentSection.childNodes);
-      } else {
-        block.replaceWith(...fragmentSection.childNodes);
-      }
-    }
+    injectFragmentOnPage(block, fragment);
   }
 }
