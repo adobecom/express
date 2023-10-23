b64c26d4cc0f19e9d9a5015a31e5655c39a2e29b
Qiyun Dai <pianoquin@gmail.com>
2023-10-23 15:03:22 -0500

    remove placeholder block code

--- express/blocks/qa-guide/qa-guide.js
+++ express/blocks/qa-guide/qa-guide.js
@@ -10,39 +10,8 @@
  * governing permissions and limitations under the License.
  */
 
-import {
-  createTag,
-} from '../../scripts/utils.js';
+import { createTag } from '../../scripts/utils.js';
 
-function setQAIndex(index, url) {
-  const usp = new URLSearchParams(url.search);
-  usp.set('qaprogress', index + 1);
-  url.search = usp.toString();
-  return decodeURIComponent(url.toString());
-}
-
-function getQAIndex() {
-  const usp = new URLSearchParams(window.location.search);
-  return Math.parseInt(usp.get('qaprogress')) - 1;
-}
 
 export default async function init(el) {
-  const pages = Array.from(el.children);
-
-  if (!pages.length) return;
-
-  const index = getQAIndex();
-
-  console.log(index);
-  if (!index) {
-    const testPage = pages[0].querySelector(':scope > div:first-of-type > a, :scope > div:first-of-type');
-    if (!testPage) {
-      el.textContent = 'Missing QA url(s)';
-      return;
-    }
-
-    const url = new URL(testPage.href || testPage.textContent);
-    const targetUrl = setQAIndex(0, url);
-    window.open(targetUrl);
-  }
 }
