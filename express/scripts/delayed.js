d421474395e5e950bf6d259bb9af3d6ddf2d484f
Jingle Huang <jinglhua@adobe.com>
2023-10-27 15:23:17 -0500

    only load product on desktop

--- express/scripts/delayed.js
+++ express/scripts/delayed.js
@@ -10,11 +10,12 @@
  * governing permissions and limitations under the License.
  */
 
-export const loadExpressProduct = async (createTag) => {
+export const loadExpressProduct = async (createTag, getDevice) => {
   if (!window.hlx.preload_product) return;
+  if (getDevice() !== 'desktop') return;
   const path = ['www.adobe.com'].includes(window.location.hostname)
     ? 'https://new.express.adobe.com/static/preload.html' : 'https://stage.projectx.corp.adobe.com/static/preload.html';
-  const iframe = createTag('iframe', { src: path, style: 'display:none' });
+  const iframe = createTag('iframe', { src: path, style: 'display:none', loading: 'lazy' });
   document.body.append(iframe);
 };
 
@@ -23,10 +24,11 @@ export const loadExpressProduct = async (createTag) => {
  */
 export default function loadDelayed([
   createTag,
+  getDevice,
 ], DELAY = 3000) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      loadExpressProduct(createTag);
+      loadExpressProduct(createTag, getDevice);
       resolve();
     }, DELAY);
   });
