fa371453f2df42c5263a2aad1cbcc23e0783994c
Jingle Huang <32369333+JingleH@users.noreply.github.com>
2023-10-27 15:32:11 -0500

    Only load product on desktop (#397)
    * only load product on desktop
    
    * no lazy load iframe
    
    ---------
    
    Co-authored-by: Jingle Huang <jinglhua@adobe.com>

--- express/scripts/delayed.js
+++ express/scripts/delayed.js
@@ -10,8 +10,9 @@
  * governing permissions and limitations under the License.
  */
 
-export const loadExpressProduct = async (createTag) => {
+export const loadExpressProduct = async (createTag, getDevice) => {
   if (!window.hlx.preload_product) return;
+  if (getDevice() !== 'desktop') return;
   const path = ['www.adobe.com'].includes(window.location.hostname)
     ? 'https://new.express.adobe.com/static/preload.html' : 'https://stage.projectx.corp.adobe.com/static/preload.html';
   const iframe = createTag('iframe', { src: path, style: 'display:none' });
@@ -23,10 +24,11 @@ export const loadExpressProduct = async (createTag) => {
  */
 export default function loadDelayed([
   createTag,
+  getDevice,
 ], DELAY = 3000) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      loadExpressProduct(createTag);
+      loadExpressProduct(createTag, getDevice);
       resolve();
     }, DELAY);
   });
