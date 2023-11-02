f7b1968c1b1d2d2a43596ca29bc9dd4d72298155
redvelvet511 <113856982+redvelvet511@users.noreply.github.com>
2023-11-02 10:43:05 -0400

    Add code for UT

--- test/unit/blocks/hero-color/hero-color.test.js
+++ test/unit/blocks/hero-color/hero-color.test.js
@@ -12,4 +12,54 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-vars */
 
-const { default: decorate } = await import('../../../../express/blocks/hero-color/hero-color.js');
+import { readFile, setViewport } from "@web/test-runner-commands";
+import { expect } from "@esm-bundle/chai";
+
+const { default: decorate, resizeSvg } = await import(
+  "../../../../express/blocks/hero-color/hero-color.js"
+);
+document.body.innerHTML = await readFile({ path: "./mocks/body.html" });
+
+describe("Hero Color", () => {
+  before(() => {
+    window.isTestEnv = true;
+    const heroColor = document.querySelector(".hero-color");
+    decorate(heroColor);
+  });
+
+  it("Should have the correct elements", () => {
+    expect(document.querySelector(".hero-color")).to.exist;
+    expect(document.querySelector(".color-svg")).to.exist;
+    expect(document.querySelector("h1")).to.exist;
+    expect(document.querySelector("p")).to.exist;
+    expect(document.querySelector(".button")).to.exist;
+  });
+
+  it("Should have a primary and secondary color", () => {
+    const svgContainer = document.querySelector(".svg-container");
+    const svgImg = document.querySelector(".color-svg-img");
+    const primaryColor = svgContainer.style.backgroundColor;
+    const secondaryColor = svgImg.style.fill;
+
+    expect(primaryColor).to.exist;
+    expect(secondaryColor).to.exist;
+  });
+
+  it("Should resize svg on load", (done) => {
+    const svg = document.querySelector(".color-svg-img");
+    // setTimeout is needed because the method has a setInterval of 50ms
+    setTimeout(() => {
+      expect(Array.from(svg.classList)).to.not.contain("hidden-svg");
+      expect(svg.style.height).to.equal("154px");
+      done();
+    }, 50);
+  });
+
+  it("Svg height should be changed after screen is resized", () => {
+    const svg = document.querySelector(".color-svg-img");
+    resizeSvg({ matches: true });
+    expect(svg.style.height).to.equal("158px");
+    resizeSvg({ matches: false });
+    expect(svg.style.height).to.equal("200px");
+  });
+});
