1492fbf5a37fc9d09407e79e5fd766dcf0dd990b
redvelvet511 <113856982+redvelvet511@users.noreply.github.com>
2023-11-03 14:11:51 -0400

    Revert "Added more test line"
    This reverts commit 171891d1c642a9f2ff2c3ad8e2343d2b4a55e98e.

--- test/unit/blocks/list/list.test.js
+++ test/unit/blocks/list/list.test.js
@@ -12,6 +12,7 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-vars */
 
+import sinon from 'sinon';
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
 import { decoratePricing } from '../../../../express/scripts/utils/pricing.js';
@@ -19,9 +20,7 @@ import { decoratePricing } from '../../../../express/scripts/utils/pricing.js';
 const { default: decorate } = await import(
   '../../../../express/blocks/list/list.js'
 );
-
-const body = await readFile({ path: './mocks/body.html' });
-const pricing = await readFile({ path: './mocks/pricing.html' });
+document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 
 describe('List', () => {
   before(() => {
@@ -29,27 +28,19 @@ describe('List', () => {
   });
 
   it('List exists', () => {
-    document.body.innerHTML = body;
     const list = document.querySelector('.list');
     decorate(list);
     expect(list).to.exist;
   });
 
   it('Should have the right elements', () => {
-    document.body.innerHTML = body;
-    const list = document.querySelector('.list');
-    decorate(list);
-
     expect(document.querySelector('.item')).to.exist;
     expect(document.querySelector('.item')).to.exist;
     expect(document.querySelector('.item-text')).to.exist;
   });
 
-  it('should handle async code', () => {
-    document.body.innerHTML = pricing;
-    const list = document.querySelector('.list');
-    decorate(list);
-
-    expect(document.querySelector('a[title^="{{pricing"]')).to.exist;
-  });
+  // it('should handle async code', async function() {
+  //     await decoratePricing();
+  //     expect(document.querySelector('a[title^='{{pricing']')).to.exist;
+  // });
 });
