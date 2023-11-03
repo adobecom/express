338f88ba8d17f3acbea487ae7277cd959fb1c405
mark <markp@adobe.com>
2023-11-03 11:36:41 -0700

    Add import

--- express/experiments/ccx0098/ch2/mobile-drawer/mobile-drawer.js
+++ express/experiments/ccx0098/ch2/mobile-drawer/mobile-drawer.js
@@ -14,6 +14,7 @@ import {
   createTag,
   getLottie,
   lazyLoadLottiePlayer,
+  transformLinkToAnimation,
   // eslint-disable-next-line import/no-unresolved
 } from '../../../../scripts/utils.js';
 
