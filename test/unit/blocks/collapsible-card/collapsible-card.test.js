c26a576a9223cdc2bd9a154b586aa2cb0bbf7eeb
redvelvet511 <113856982+redvelvet511@users.noreply.github.com>
2023-11-06 15:04:51 -0500

    Added UT

--- test/unit/blocks/collapsible-card/collapsible-card.test.js
+++ test/unit/blocks/collapsible-card/collapsible-card.test.js
@@ -12,4 +12,51 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-vars */
 
-const { default: decorate } = await import('../../../../express/blocks/collapsible-card/collapsible-card.js');
+import { readFile } from '@web/test-runner-commands';
+import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
+
+const { default: decorate } = await import(
+  '../../../../express/blocks/collapsible-card/collapsible-card.js'
+);
+document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+const clock = sinon.useFakeTimers({ shouldAdvanceTime: true });
+
+describe('Collapsible Card', () => {
+  before(async () => {
+    window.isTestEnv = true;
+    const collapsibleCard = document.querySelector('.collapsible-card');
+    await decorate(collapsibleCard);
+  });
+
+  after(() => {
+    clock.restore();
+  });
+
+  it('Collapsible Card exists', () => {
+    const collapsibleCard = document.querySelector('.collapsible-card');
+    decorate(collapsibleCard);
+    expect(collapsibleCard).to.exist;
+  });
+
+  it('If toggle expanded', async () => {
+    const toggleButton = document.querySelector('.toggle-button');
+    toggleButton.click();
+
+    await clock.nextAsync();
+    expect(document.querySelector('.collapsible-card').classList.contains('expanded')).to.be.true;
+  });
+
+  it('If no toggle expanded', async () => {
+    const toggleButton = document.querySelector('.toggle-button');
+    toggleButton.click();
+
+    await clock.nextAsync();
+    expect(document.querySelector('.collapsible-card').classList.contains('expanded')).to.be.false;
+  });
+
+  it('Anchor has right class', async () => {
+    const anchor = document.querySelector('a');
+    expect(anchor.classList.contains('badge')).to.be.true;
+  });
+});
