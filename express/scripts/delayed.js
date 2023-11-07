3ba79143d6557e566af01619d4f0e93af73dcacb
Qiyun Dai <pianoquin@gmail.com>
2023-11-07 14:54:21 -0600

    remove bmtp

--- express/scripts/delayed.js
+++ express/scripts/delayed.js
@@ -11,10 +11,8 @@
  */
 
 import {
-  fetchPlaceholders,
   createTag,
   getDevice,
-  getMetadata,
 } from './utils.js';
 
 export const loadExpressProduct = async () => {
@@ -26,37 +24,14 @@ export const loadExpressProduct = async () => {
   document.body.append(iframe);
 };
 
-async function isLoggedInDelayed() {
-  const userProfile = window.adobeProfile?.getUserProfile();
-  const placeholders = await fetchPlaceholders();
-  const autoRedirect = ['yes', 'true', 'Y', 'on'].includes(getMetadata('direct-path-to-product'));
-  const autoRedirectLanguageFound = placeholders.cancel || placeholders['bmtp-header'] || placeholders['bmtp-cancel-text'];
-  const isDesktop = getDevice() !== 'desktop';
-
-  return userProfile && autoRedirect && autoRedirectLanguageFound && isDesktop;
-}
-
 /**
  * Executes everything that happens a lot later, without impacting the user experience.
  */
 export default function loadDelayed(DELAY = 3000) {
-  isLoggedInDelayed().then(async (bringMeToProduct) => {
-    if (bringMeToProduct) {
-      const { default: loadLoginUserAutoRedirect } = await import('../features/direct-path-to-product/direct-path-to-product.js');
-      return new Promise((resolve) => {
-        loadExpressProduct();
-        setTimeout(() => {
-          loadLoginUserAutoRedirect();
-          resolve();
-        }, DELAY);
-      });
-    }
-
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        loadExpressProduct();
-        resolve();
-      }, DELAY);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      loadExpressProduct();
+      resolve();
+    }, DELAY);
   });
 }
