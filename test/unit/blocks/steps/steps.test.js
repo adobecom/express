f691430850fd1dadeaccdcf54187eda6f9fc3480
sandra <113856982+redvelvet511@users.noreply.github.com>
2023-11-08 12:27:28 -0500

    Added UT (#426)

--- test/unit/blocks/steps/steps.test.js
+++ test/unit/blocks/steps/steps.test.js
@@ -12,4 +12,62 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-vars */
 
-const { default: decorate } = await import('../../../../express/blocks/steps/steps.js');
+import { readFile } from '@web/test-runner-commands';
+import { expect } from '@esm-bundle/chai';
+
+const { default: decorate } = await import(
+  '../../../../express/blocks/steps/steps.js'
+);
+
+const body = await readFile({ path: './mocks/body.html' });
+const schemaHighlight = await readFile({ path: './mocks/schema-highlight.html' });
+const schemaDark = await readFile({ path: './mocks/schema-dark.html' });
+const schema = await readFile({ path: './mocks/schema.html' });
+
+describe('Steps', () => {
+  before(() => {
+    window.isTestEnv = true;
+  });
+
+  it('Steps exist', () => {
+    document.body.innerHTML = body;
+    const steps = document.querySelector('.steps');
+    decorate(steps);
+    expect(steps).to.exist;
+  });
+
+  it('Steps has the right elements', () => {
+    document.body.innerHTML = body;
+    const steps = document.querySelector('.steps');
+    decorate(steps);
+
+    expect(document.querySelector('.section')).to.exist;
+    expect(document.querySelector('h3')).to.exist;
+    expect(document.querySelector('.step-description')).to.exist;
+  });
+
+  it('Highlight schema contains right elements', () => {
+    document.body.innerHTML = schemaHighlight;
+    const steps = document.querySelector('.steps');
+    decorate(steps);
+
+    expect(document.querySelector('.steps-highlight-container')).to.exist;
+  });
+
+  it('Dark schema contains right elements', () => {
+    document.body.innerHTML = schemaDark;
+    const steps = document.querySelector('.steps');
+    decorate(steps);
+
+    expect(document.querySelector('.steps-dark-container')).to.exist;
+  });
+
+  it('Schema has the right elements', () => {
+    document.body.innerHTML = schema;
+    const steps = document.querySelector('.steps');
+    decorate(steps);
+
+    expect(document.querySelector('.steps-container')).to.exist;
+    expect(document.querySelector('script')).to.exist;
+  });
+});
