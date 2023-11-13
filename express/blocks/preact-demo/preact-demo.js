9a20964a464c053ba802968df4cb238bf25f90ea
Jingle Huang <32369333+JingleH@users.noreply.github.com>
2023-11-13 14:46:44 -0800

    Add Preact Scripts (#463)
    * added demos
    
    * no cls
    
    * no loadCSS
    
    * as lcp
    
    * preset width height
    
    * cleanups
    
    * lint
    
    ---------
    
    Co-authored-by: Jingle Huang <jinglhua@adobe.com>

---
+++ express/blocks/preact-demo/preact-demo.js
@@ -0,0 +1,24 @@
+import { createTag } from '../../scripts/utils.js';
+import { html, render } from '../../scripts/libs/htm-preact.js';
+import Game from './ttt.js';
+import Temperature from './temperature.js';
+
+export default async function init(el) {
+  const img = el.querySelector('img');
+  img.width = 600;
+  img.height = 300;
+  const demoContainer = createTag('div', { class: 'demo-container' });
+  const ttt = html`
+    <${Game} />
+  `;
+  const temperature = html`
+    <${Temperature} />
+  `;
+  const tttDiv = createTag('div');
+  const temperatureDiv = createTag('div');
+  render(ttt, tttDiv);
+  render(temperature, temperatureDiv);
+  demoContainer.append(tttDiv);
+  demoContainer.append(temperatureDiv);
+  el.append(demoContainer);
+}
