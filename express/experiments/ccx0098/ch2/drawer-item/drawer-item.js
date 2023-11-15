0bc7b405b1f2700133227f55c35e88562e935835
mark <markp@adobe.com>
2023-11-14 18:37:18 -0800

    Uncomment

--- express/experiments/ccx0098/ch2/drawer-item/drawer-item.js
+++ express/experiments/ccx0098/ch2/drawer-item/drawer-item.js
@@ -447,9 +447,9 @@ function initAfterDrawerFullyLoaded(payload) {
     decorateBubblesView(payload);
     if (payload.hasList) {
       decorateIconCTAContainer(payload, '.drawer-item-icon-cta-container');
-      // const [heading, sortedIcons] =  [[...payload.iconContainer.children][0],[...payload.drawerItemContainer].slice(1).sort((a,b) =>  a.dataset.order - b.dataset.order)];
-      // console.log('heading',heading);
-      // console.log('sortedIcons',sortedIcons);
+      const [heading, sortedIcons] =  [[...payload.iconContainer.children][0],[...payload.drawerItemContainer].slice(1).sort((a,b) =>  a.dataset.order - b.dataset.order)];
+      console.log('heading',heading);
+      console.log('sortedIcons',sortedIcons);
       // console.log('payload.iconContainer',payload.iconContainer);
 
       // payload.iconContainer = '';
