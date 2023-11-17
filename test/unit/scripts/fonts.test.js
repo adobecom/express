074f12ed02c560a94424c57b860fcdde865eeb34
Qiyun Dai <pianoquin@gmail.com>
2023-11-17 11:19:36 -0600

    fixing unit test

--- test/unit/scripts/fonts.test.js
+++ test/unit/scripts/fonts.test.js
@@ -6,13 +6,13 @@ import { loadStyle } from '../../../express/scripts/utils.js';
 describe('Fonts', () => {
   it('Loads CSS fonts', async () => {
     const { default: loadFonts } = await import('../../../express/scripts/fonts.js');
-    const fontResp = await loadFonts('hah7vzn.css', loadStyle);
+    const fontResp = await loadFonts({ ietf: 'en-US', tk: 'jdq5hay.css' }, loadStyle);
     expect(fontResp).to.exist;
   });
 
   it('Loads JS fonts', async () => {
     const { default: loadFonts } = await import('../../../express/scripts/fonts.js');
-    const fontResp = await loadFonts('hah7vzn', loadStyle);
+    const fontResp = await loadFonts({ ietf: 'ja-JP', tk: 'dvg6awq' }, loadStyle);
     expect(fontResp.classList.contains('wf-loading')).to.be.true;
   });
 });
