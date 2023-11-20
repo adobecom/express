23b8e5a2afb8c083197045a9926685a952bb170f
Qiyun Dai <pianoquin@gmail.com>
2023-11-20 10:44:47 -0600

    Carousel CLS fix

--- express/blocks/shared/carousel.js
+++ express/blocks/shared/carousel.js
@@ -60,124 +60,130 @@ export function initToggleTriggers(parent) {
 }
 
 export default async function buildCarousel(selector, parent, options = {}) {
-  // Load CSS
-  loadCSS('/express/blocks/shared/carousel.css');
-  // Build the carousel HTML
-  const carouselContent = selector ? parent.querySelectorAll(selector) : parent.querySelectorAll(':scope > *');
+  parent.style.visibility = 'hidden';
 
-  carouselContent.forEach((el) => el.classList.add('carousel-element'));
+  const onCarouselCSSLoad = () => {
+    const carouselContent = selector ? parent.querySelectorAll(selector) : parent.querySelectorAll(':scope > *');
 
-  const container = createTag('div', { class: 'carousel-container' });
-  const platform = createTag('div', { class: 'carousel-platform' });
+    carouselContent.forEach((el) => el.classList.add('carousel-element'));
 
-  const leftTrigger = createTag('div', { class: 'carousel-left-trigger' });
-  const rightTrigger = createTag('div', { class: 'carousel-right-trigger' });
+    const container = createTag('div', { class: 'carousel-container' });
+    const platform = createTag('div', { class: 'carousel-platform' });
 
-  const faderLeft = createTag('div', { class: 'carousel-fader-left arrow-hidden' });
-  const faderRight = createTag('div', { class: 'carousel-fader-right arrow-hidden' });
+    const leftTrigger = createTag('div', { class: 'carousel-left-trigger' });
+    const rightTrigger = createTag('div', { class: 'carousel-right-trigger' });
 
-  const arrowLeft = createTag('a', { class: 'button carousel-arrow carousel-arrow-left' });
-  const arrowRight = createTag('a', { class: 'button carousel-arrow carousel-arrow-right' });
+    const faderLeft = createTag('div', { class: 'carousel-fader-left arrow-hidden' });
+    const faderRight = createTag('div', { class: 'carousel-fader-right arrow-hidden' });
 
-  platform.append(leftTrigger, ...carouselContent, rightTrigger);
-  container.append(platform, faderLeft, faderRight);
-  faderLeft.append(arrowLeft);
-  faderRight.append(arrowRight);
-  parent.append(container);
+    const arrowLeft = createTag('a', { class: 'button carousel-arrow carousel-arrow-left' });
+    const arrowRight = createTag('a', { class: 'button carousel-arrow carousel-arrow-right' });
 
-  // If flex container has a gap, add negative margins to compensate
-  const gap = window.getComputedStyle(platform, null).getPropertyValue('gap');
-  if (gap !== 'normal') {
-    const gapInt = parseInt(gap.replace('px', ''), 10);
-    leftTrigger.style.marginRight = `-${gapInt + 1}px`;
-    rightTrigger.style.marginLeft = `-${gapInt + 1}px`;
-  }
+    platform.append(leftTrigger, ...carouselContent, rightTrigger);
+    container.append(platform, faderLeft, faderRight);
+    faderLeft.append(arrowLeft);
+    faderRight.append(arrowRight);
+    parent.append(container);
 
-  // Scroll the carousel by clicking on the controls
-  const moveCarousel = (increment) => {
-    platform.scrollLeft -= increment;
-  };
-
-  faderLeft.addEventListener('click', () => {
-    const increment = Math.max((platform.offsetWidth / 4) * 3, 300);
-    moveCarousel(increment);
-  });
-  faderRight.addEventListener('click', () => {
-    const increment = Math.max((platform.offsetWidth / 4) * 3, 300);
-    moveCarousel(-increment);
-  });
-
-  // Carousel loop functionality (if enabled)
-  const stopScrolling = () => { // To prevent safari shakiness
-    platform.style.overflowX = 'hidden';
-    setTimeout(() => {
-      platform.style.removeProperty('overflow-x');
-    }, 20);
-  };
-
-  const moveToCenterIfNearTheEdge = (e = null) => {
-    // Start at the center and snap back to center if the user scrolls to the edges
-    const scrollPos = platform.scrollLeft;
-    const maxScroll = platform.scrollWidth;
-    if ((scrollPos > (maxScroll / 5) * 4) || scrollPos < 30) {
-      if (e) e.preventDefault();
-      stopScrolling();
-      platform.scrollTo({
-        left: ((maxScroll / 5) * 2),
-        behavior: 'instant',
-      });
+    // If flex container has a gap, add negative margins to compensate
+    const gap = window.getComputedStyle(platform, null).getPropertyValue('gap');
+    if (gap !== 'normal') {
+      const gapInt = parseInt(gap.replace('px', ''), 10);
+      leftTrigger.style.marginRight = `-${gapInt + 1}px`;
+      rightTrigger.style.marginLeft = `-${gapInt + 1}px`;
     }
-  };
 
-  const infinityScroll = (children) => {
-    const duplicateContent = () => {
-      [...children].forEach((child) => {
-        const duplicate = child.cloneNode(true);
-        const duplicateLinks = duplicate.querySelectorAll('a');
-        platform.append(duplicate);
-        if (duplicate.tagName.toLowerCase() === 'a') {
-          const linksPopulated = new CustomEvent('linkspopulated', { detail: [duplicate] });
-          document.dispatchEvent(linksPopulated);
-        }
-        if (duplicateLinks) {
-          const linksPopulated = new CustomEvent('linkspopulated', { detail: duplicateLinks });
-          document.dispatchEvent(linksPopulated);
-        }
-      });
+    // Scroll the carousel by clicking on the controls
+    const moveCarousel = (increment) => {
+      platform.scrollLeft -= increment;
     };
 
-    // Duplicate children to simulate smooth scrolling
-    for (let i = 0; i < 4; i += 1) {
-      duplicateContent();
-    }
+    faderLeft.addEventListener('click', () => {
+      const increment = Math.max((platform.offsetWidth / 4) * 3, 300);
+      moveCarousel(increment);
+    });
+    faderRight.addEventListener('click', () => {
+      const increment = Math.max((platform.offsetWidth / 4) * 3, 300);
+      moveCarousel(-increment);
+    });
 
-    platform.addEventListener('scroll', (e) => {
-      moveToCenterIfNearTheEdge(e);
-    }, { passive: false });
-  };
-
-  // set initial states
-  const setInitialState = (scrollable, opts) => {
-    if (opts.infinityScrollEnabled) {
-      infinityScroll([...carouselContent]);
-      faderLeft.classList.remove('arrow-hidden');
-      faderRight.classList.remove('arrow-hidden');
-      platform.classList.add('left-fader', 'right-fader');
-    }
-
-    const onIntersect = ([entry], observer) => {
-      if (!entry.isIntersecting) return;
-
-      if (opts.centerAlign) correctCenterAlignment(scrollable);
-      if (opts.startPosition === 'right') moveCarousel(-scrollable.scrollWidth);
-      if (!opts.infinityScrollEnabled) initToggleTriggers(container);
-
-      observer.unobserve(scrollable);
+    // Carousel loop functionality (if enabled)
+    const stopScrolling = () => { // To prevent safari shakiness
+      platform.style.overflowX = 'hidden';
+      setTimeout(() => {
+        platform.style.removeProperty('overflow-x');
+      }, 20);
     };
 
-    const carouselObserver = new IntersectionObserver(onIntersect, { threshold: 0 });
-    carouselObserver.observe(scrollable);
+    const moveToCenterIfNearTheEdge = (e = null) => {
+      // Start at the center and snap back to center if the user scrolls to the edges
+      const scrollPos = platform.scrollLeft;
+      const maxScroll = platform.scrollWidth;
+      if ((scrollPos > (maxScroll / 5) * 4) || scrollPos < 30) {
+        if (e) e.preventDefault();
+        stopScrolling();
+        platform.scrollTo({
+          left: ((maxScroll / 5) * 2),
+          behavior: 'instant',
+        });
+      }
+    };
+
+    const infinityScroll = (children) => {
+      const duplicateContent = () => {
+        [...children].forEach((child) => {
+          const duplicate = child.cloneNode(true);
+          const duplicateLinks = duplicate.querySelectorAll('a');
+          platform.append(duplicate);
+          if (duplicate.tagName.toLowerCase() === 'a') {
+            const linksPopulated = new CustomEvent('linkspopulated', { detail: [duplicate] });
+            document.dispatchEvent(linksPopulated);
+          }
+          if (duplicateLinks) {
+            const linksPopulated = new CustomEvent('linkspopulated', { detail: duplicateLinks });
+            document.dispatchEvent(linksPopulated);
+          }
+        });
+      };
+
+      // Duplicate children to simulate smooth scrolling
+      for (let i = 0; i < 4; i += 1) {
+        duplicateContent();
+      }
+
+      platform.addEventListener('scroll', (e) => {
+        moveToCenterIfNearTheEdge(e);
+      }, { passive: false });
+    };
+
+    // set initial states
+    const setInitialState = (scrollable, opts) => {
+      if (opts.infinityScrollEnabled) {
+        infinityScroll([...carouselContent]);
+        faderLeft.classList.remove('arrow-hidden');
+        faderRight.classList.remove('arrow-hidden');
+        platform.classList.add('left-fader', 'right-fader');
+      }
+
+      const onIntersect = ([entry], observer) => {
+        if (!entry.isIntersecting) return;
+
+        if (opts.centerAlign) correctCenterAlignment(scrollable);
+        if (opts.startPosition === 'right') moveCarousel(-scrollable.scrollWidth);
+        if (!opts.infinityScrollEnabled) initToggleTriggers(container);
+
+        observer.unobserve(scrollable);
+      };
+
+      const carouselObserver = new IntersectionObserver(onIntersect, { threshold: 0 });
+      carouselObserver.observe(scrollable);
+    };
+
+    setInitialState(platform, options);
+
+    parent.style.removeProperty('visibility');
   };
 
-  setInitialState(platform, options);
+  // Load CSS then build carousel
+  loadCSS('/express/blocks/shared/carousel.css', onCarouselCSSLoad);
 }
