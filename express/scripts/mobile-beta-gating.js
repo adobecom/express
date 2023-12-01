ef6025dc39af3b4f7aaf713fce450c75442b8602
Qiyun Dai <pianoquin@gmail.com>
2023-12-01 11:06:26 -0600

    testing cookie setting

--- express/scripts/mobile-beta-gating.js
+++ express/scripts/mobile-beta-gating.js
@@ -1,38 +1,98 @@
 import { BlockMediator } from './block-mediator.min.js';
 import { getMobileOperatingSystem } from './utils.js';
 
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+function setSessionCookie(name, value, domain) {
+  let cookie = '';
+
+  cookie = `${name}=${value};`;
+
+  if (domain) {
+    cookie += ` domain=${domain};`;
+  }
+
+  cookie += 'path=/';
+
+  document.cookie = cookie;
 }
 
-export default async function checkMobileBetaEligibility() {
+function getCookie(name) {
+  const nameEQ = `${name}=`;
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i += 1) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+  }
+  return null;
+}
+
+function isIOS16AndUp() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/iPhone/i.test(userAgent)) {
+    const iOSVersionMatch = userAgent.match(/OS (\d+)_/);
+    if (iOSVersionMatch && iOSVersionMatch.length > 1) {
+      const iOSVersion = parseInt(iOSVersionMatch[1], 10);
+
+      return iOSVersion >= 16;
+    }
+  }
+
+  return false;
+}
+
+function runBenchmark() {
   if (window.Worker) {
     const benchmarkWorker = new Worker('/express/scripts/gating-benchmark.js');
     benchmarkWorker.postMessage(10000000);
     benchmarkWorker.onmessage = (e) => {
       const criterion = {
         cpuSpeedPass: e.data <= 400,
-        cpuCoreCountPass: (navigator.hardwareConcurrency
-          && navigator.hardwareConcurrency >= 4)
-        || false,
       };
-  
-      if (getMobileOperatingSystem() !== 'iOS') {
-        criterion.memoryCapacityPass = (navigator.deviceMemory
-          && navigator.deviceMemory >= 4)
-        || false;
+
+      // old android approach
+      // if (getMobileOperatingSystem() !== 'iOS') {
+      //   criterion.cpuCoreCountPass = (navigator.hardwareConcurrency
+      //     && navigator.hardwareConcurrency >= 4)
+      //   || false;
+      //   criterion.memoryCapacityPass = (navigator.deviceMemory
+      //     && navigator.deviceMemory >= 4)
+      //   || false;
+      // }
+
+      if (getMobileOperatingSystem() === 'iOS') {
+        criterion.iOSVersionPass = isIOS16AndUp();
       }
-  
+
       const deviceEligible = Object.values(criterion).every((criteria) => criteria);
-  
+
       BlockMediator.set('mobileBetaEligibility', {
-        result: deviceEligible ? 'passed' : 'rejected',
+        deviceSupport: deviceEligible ? 'true' : 'false',
         data: criterion,
       });
-  
+
       benchmarkWorker.terminate();
     };
+  }
+}
+
+export default async function checkMobileBetaEligibility() {
+  const deviceSupportCookie = getCookie('device-support');
+
+  if (deviceSupportCookie) {
+    BlockMediator.set('mobileBetaEligibility', {
+      deviceSupport: deviceSupportCookie,
+      data: {
+        reason: 'pre-checked',
+      },
+    });
+  } else {
+    runBenchmark();
+    const unsubscribe = BlockMediator.subscribe('mobileBetaEligibility', async (e) => {
+      if (['true', 'false'].includes(e.newValue.deviceSupport)) {
+        setSessionCookie('device-support', e.newValue.deviceSupport, '.adobe.com');
+        unsubscribe();
+      }
+    });
   }
 }
