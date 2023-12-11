c1756588c2e644999a8122cc6c1413442e9210f9
Jingle Huang <jinglhua@adobe.com>
2023-12-11 14:06:14 -0800

    no preload-product on pep

--- express/scripts/delayed.js
+++ express/scripts/delayed.js
@@ -4,7 +4,7 @@ import {
   getMetadata,
 } from './utils.js';
 
-export const loadExpressProduct = async () => {
+export const loadExpressProduct = () => {
   if (!window.hlx.preload_product) return;
   if (document.body.dataset.device === 'mobile') return;
   const path = ['www.adobe.com'].includes(window.location.hostname)
@@ -18,32 +18,35 @@ async function isLoggedInDelayed() {
   const placeholders = await fetchPlaceholders();
   const autoRedirect = ['yes', 'true', 'Y', 'on'].includes(getMetadata('direct-path-to-product'));
   const autoRedirectLanguageFound = placeholders.cancel || placeholders['bmtp-header'] || placeholders['bmtp-cancel-text'];
-  const isDesktop = document.body.dataset.device !== 'desktop';
+  const isDesktop = document.body.dataset.device === 'desktop';
 
   return userProfile && autoRedirect && autoRedirectLanguageFound && isDesktop;
 }
 
+const PEP_DELAY = 3000;
+
 /**
  * Executes everything that happens a lot later, without impacting the user experience.
  */
-export default function loadDelayed(DELAY = 3000) {
-  isLoggedInDelayed().then(async (bringMeToProduct) => {
-    if (bringMeToProduct) {
-      const { default: loadLoginUserAutoRedirect } = await import('../features/direct-path-to-product/direct-path-to-product.js');
-      return new Promise((resolve) => {
-        loadExpressProduct();
-        setTimeout(() => {
-          loadLoginUserAutoRedirect();
-          resolve();
-        }, DELAY);
-      });
-    }
-
+export default async function loadDelayed(DELAY = 3000) {
+  const bringMeToProduct = await isLoggedInDelayed();
+  if (bringMeToProduct) {
+    const { default: loadLoginUserAutoRedirect } = await import('../features/direct-path-to-product/direct-path-to-product.js');
     return new Promise((resolve) => {
+      // TODO: not preloading product this early to protect desktop CWV
+      // until we see significant proof of preloading improving product load time
+      // loadExpressProduct();
       setTimeout(() => {
-        loadExpressProduct();
+        loadLoginUserAutoRedirect();
         resolve();
-      }, window.delay_preload_product ? DELAY * 2 : DELAY);
+      }, PEP_DELAY);
     });
+  }
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      loadExpressProduct();
+      resolve();
+    }, window.delay_preload_product ? DELAY * 2 : DELAY);
   });
 }
