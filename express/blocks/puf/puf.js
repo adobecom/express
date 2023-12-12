c7cfd273c5902d63ea2f511dd06f4ad6921c5330
Aaron Atkinson <38330431+AaronAtkinson@users.noreply.github.com>
2023-12-12 15:53:25 -0500

    Lowered the intersection threshold
    Lowered the intersection threshold even more from 75% to 55%
    (ideal passing 50% would change the height, but with the gap it looks a tad early to me)

--- express/blocks/puf/puf.js
+++ express/blocks/puf/puf.js
@@ -407,7 +407,7 @@ async function build2ColDesign(block) {
   const options = {
     root: document.querySelector('.carousel-platform'),
     rootMargin: '0px',
-    threshold: 0.75,
+    threshold: 0.55,
   };
   const carouselContainer = block.querySelector('.carousel-container');
   const callback = (entries) => {
