2ebd698388a8007b923875c09beae47d365293cc
Jingle Huang <jinglhua@adobe.com>
2023-12-13 14:37:25 -0800

    removed carousel fader

--- express/blocks/template-x/template-x.js
+++ express/blocks/template-x/template-x.js
@@ -1097,6 +1097,7 @@ function initExpandCollapseToolbar(block, templateTitle, toggle, link) {
 }
 
 function decorateHoliday(block, props) {
+  console.log({ block, props });
   const templateTitle = block.querySelector('.template-title');
   const textWrapper = templateTitle.querySelector('.text-wrapper');
   const textWrapperInnerDiv = textWrapper.querySelector(':scope > div');
@@ -1108,24 +1109,12 @@ function decorateHoliday(block, props) {
   const topElements = createTag('div', { class: 'toggle-bar-top' });
   const bottomElements = createTag('div', { class: 'toggle-bar-bottom' });
   const toggleChev = createTag('div', { class: 'toggle-button-chev' });
-  const carouselFaderLeft = block.querySelector('.carousel-fader-left');
-  const carouselFaderRight = block.querySelector('.carousel-fader-right');
 
   if (props.holidayIcon) topElements.append(props.holidayIcon);
   if (props.backgroundAnimation) {
     const animation = transformLinkToAnimation(props.backgroundAnimation);
     block.classList.add('animated');
     block.prepend(animation);
-  }
-
-  if (props.backgroundColor) {
-    if (props.backgroundAnimation) {
-      carouselFaderRight.style.backgroundImage = 'none';
-      carouselFaderLeft.style.backgroundImage = 'none';
-    } else {
-      carouselFaderRight.style.backgroundImage = `linear-gradient(to right, rgba(0, 255, 255, 0), ${props.backgroundColor}`;
-      carouselFaderLeft.style.backgroundImage = `linear-gradient(to left, rgba(0, 255, 255, 0), ${props.backgroundColor}`;
-    }
   }
 
   block.classList.add('expanded', props.textColor);
