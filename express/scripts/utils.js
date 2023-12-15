b86195424dc4f4eee7db1f9c5274771171dbbd6b
David Catalan <catalan@adobe.com>
2023-12-15 09:31:14 +0100

    fix: remove duplicated function

--- express/scripts/utils.js
+++ express/scripts/utils.js
@@ -2550,25 +2550,6 @@ export async function loadSections(sections, isDoc) {
   return areaBlocks;
 }
 
-export function getMobileOperatingSystem() {
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-  // Windows Phone must come first because its UA also contains "Android"
-  if (/windows phone/i.test(userAgent)) {
-    return 'Windows Phone';
-  }
-
-  if (/android/i.test(userAgent)) {
-    return 'Android';
-  }
-
-  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    return 'iOS';
-  }
-
-  return 'unknown';
-}
-
 function initSidekick() {
   const initPlugins = async () => {
     const { default: init } = await import('./utils/sidekick.js');
