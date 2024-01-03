63cd1f1d0f625e400bb7ddc3597d917305494254
redvelvet511 <113856982+redvelvet511@users.noreply.github.com>
2024-01-03 11:31:13 -0500

    Remove issue #2 changes to separate

--- express/blocks/columns/columns.js
+++ express/blocks/columns/columns.js
@@ -261,11 +261,9 @@ export default async function decorate(block) {
     }
   }
 
-  // add free plan widget to first columns block on every page except blog
-
-  if (!window.location.href.includes('/express/learn/blog') && document.querySelector('main .columns') === block && document.querySelector('main .block') === block) {
+  // add free plan widget to first columns block on every page
+  if (document.querySelector('main .columns') === block && document.querySelector('main .block') === block) { 
     addFreePlanWidget(block.querySelector('.button-container') || block.querySelector(':scope .column:not(.hero-animation-overlay,.columns-picture)'));
-  }
 
   // invert buttons in regular columns inside columns-highlight-container
   if (block.closest('.section.columns-highlight-container') && !block.classList.contains('highlight')) {
