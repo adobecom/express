690a59b6081ab1ae0bae74a7048f580b47744c38
Jingle Huang <jinglhua@adobe.com>
2024-01-02 22:47:12 -0600

    fix tests

--- test/unit/scripts/mobile-beta-gating.test.js
+++ test/unit/scripts/mobile-beta-gating.test.js
@@ -1,4 +1,5 @@
 import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
 import checkMobileBetaEligibility, {
   isIOS16AndUp,
   isOfficiallySupportedDevice,
@@ -28,23 +29,138 @@ describe('Mobile Beta Gating', () => {
   });
 
   it('is able to check iOS versions', async () => {
-    expect(isIOS16AndUp('Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1')).to.be.true;
-    expect(isIOS16AndUp('Mozilla/5.0 (Linux; Android 13; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36')).to.be.false;
+    expect(
+      isIOS16AndUp(
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+      ),
+    ).to.be.true;
+    expect(
+      isIOS16AndUp(
+        'Mozilla/5.0 (Linux; Android 13; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+      ),
+    ).to.be.false;
   });
 
   it('is able to check whitelist', async () => {
+    const MOCK_JSON = {
+      total: 20,
+      offset: 0,
+      limit: 20,
+      data: [
+        {
+          device: 'SM-S908E',
+        },
+        {
+          device: '2201117TI',
+        },
+        {
+          device: 'SM-G781B',
+        },
+        {
+          device: 'M2102J20SI',
+        },
+        {
+          device: 'SM-G980F',
+        },
+        {
+          device: 'SM-G955U',
+        },
+        {
+          device: 'SM-A546E',
+        },
+        {
+          device: 'RMX2001',
+        },
+        {
+          device: 'CPH2401',
+        },
+        {
+          device: 'SM-N980F',
+        },
+        {
+          device: 'M2012K11AI',
+        },
+        {
+          device: 'NE2211',
+        },
+        {
+          device: 'Pixel 2 XL',
+        },
+        {
+          device: 'Samsung M525G',
+        },
+        {
+          device: 'Samsung Galaxy S23 Ultra',
+        },
+        {
+          device: 'Samsung Galaxy A53 5G',
+        },
+        {
+          device: 'Redmi Note 8 Pro',
+        },
+        {
+          device: 'Samsung Galaxy S10',
+        },
+        {
+          device: 'Samsung Galaxy A71',
+        },
+        {
+          device: 'Pixel 6 Pro',
+        },
+      ],
+      ':type': 'sheet',
+    };
+    const stub = sinon.stub(window, 'fetch');
+    stub.returns({ ok: true, json: async () => MOCK_JSON });
     Object.defineProperty(navigator, 'userAgent', {
       get() {
         return 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1';
       },
     });
-    expect(isOfficiallySupportedDevice('iOS')).to.be.true;
+    let supported = await isOfficiallySupportedDevice('iOS');
+    expect(supported).to.be.true;
     Object.defineProperty(navigator, 'userAgent', {
       get() {
         return 'Mozilla/5.0 (Linux; Android 13; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36';
       },
     });
-    expect(isOfficiallySupportedDevice('android')).to.be.false;
-    expect(isOfficiallySupportedDevice('windows')).to.be.false;
+    supported = await isOfficiallySupportedDevice('Android');
+    expect(supported).to.be.false;
+
+    Object.defineProperty(navigator, 'userAgent', {
+      get() {
+        return 'Mozilla/5.0 (Linux; Android 13; SM-G955U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36';
+      },
+    });
+    supported = await isOfficiallySupportedDevice('Android');
+    expect(supported).to.be.true;
+
+    Object.defineProperty(navigator, 'userAgent', {
+      get() {
+        return 'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36';
+      },
+    });
+    supported = await isOfficiallySupportedDevice('Android');
+    expect(supported).to.be.false;
+
+    Object.defineProperty(navigator, 'userAgent', {
+      get() {
+        return 'Mozilla/5.0 (Linux; Android 13; Pixel 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36';
+      },
+    });
+    supported = await isOfficiallySupportedDevice('Android');
+    expect(supported).to.be.true;
+
+    Object.defineProperty(navigator, 'userAgent', {
+      get() {
+        return 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36';
+      },
+    });
+    supported = await isOfficiallySupportedDevice('Android');
+    expect(supported).to.be.true;
+
+    supported = await isOfficiallySupportedDevice('windows');
+    expect(supported).to.be.false;
+    stub.restore();
   });
 });
