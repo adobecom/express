955f86eeb7e95f8e0f31ae0ad78506a5890b728f
Victor Hargrave <vhargrave@adobe.com>
2024-01-05 15:57:34 +0100

    MWPW-140480 try initiating quick action on button click

--- express/blocks/frictionless-quick-action/frictionless-quick-action.js
+++ express/blocks/frictionless-quick-action/frictionless-quick-action.js
@@ -1,6 +1,6 @@
-import { loadScript } from '../../scripts/utils.js';
+import { createTag, loadScript } from '../../scripts/utils.js';
 
-export default function decorate(block) {
+function startSDK(data) {
   const CDN_URL = 'https://sdk.cc-embed.adobe.com/v3/CCEverywhere.js';
   loadScript(CDN_URL).then(async () => {
     if (!window.CCEverywhere) {
@@ -11,11 +11,96 @@ export default function decorate(block) {
       appName: 'express',
     });
 
+    const exportOptions = [
+      /* This native button renders label "Open in Adobe Express" */
+      {
+        target: 'Editor',
+        id: 'edit-in-express',
+        buttonType: 'native',
+        optionType: 'button'
+      },
+      /* This native button renders label "Download" */
+      {
+        target: 'Download',
+        id: 'download-button',
+        optionType: 'button',
+        buttonType: 'native',
+      },
+      {
+        target: 'Host',
+        id: 'my-custom-button',
+        label: 'Embed in app',
+        closeTargetOnExport: true,
+        optionType: 'button',
+        buttonType: 'custom',
+      },
+    ];
+
+    const imageCallbacks = {
+      onPublish: (publishParams) => {
+        // Handle custom export button behavior here
+        debugger;
+        if (publishParams.exportButtonId=="my-custom-button"){
+          const localData = { asset: publishParams.asset[0].data }
+          appImage.src = localData.asset;
+        }
+      },
+    };
+
     console.log('opening crop image quick action');
     ccEverywhere.openQuickAction({
-      id: 'crop-image',
+      id: 'remove-background',
+      inputParams: {
+        asset: {
+          data,
+          dataType: 'base64',
+          type: 'image',
+        },
+        exportOptions,
+      },
+      callbacks: imageCallbacks
     });
+
   });
   // eslint-disable-next-line no-console
   console.log('quick action worked');
+}
+
+function uploadFile() {
+  // Create an input element
+  let inputElement = document.createElement('input');
+  inputElement.type = 'file';
+
+  // Trigger the file selector when the button is clicked
+  inputElement.click();
+
+  // Handle file selection
+  inputElement.onchange = function() {
+    let file = inputElement.files[0];
+    let reader = new FileReader();
+
+    reader.onloadend = function() {
+      // The file content is in reader.result as a Base64 encoded string
+      console.log('Base64 string:', reader.result);
+      startSDK(reader.result);
+      // You can add additional code here to use the Base64 string
+    };
+
+    // Read the file as a data URL (Base64)
+    reader.readAsDataURL(file);
+  };
+}
+
+export default function decorate(block) {
+
+
+  const button = createTag('button', {}, 'remove background');
+
+  button.addEventListener("click", () => {
+    uploadFile();
+  });
+
+  block.append(button);
+
+
 }
