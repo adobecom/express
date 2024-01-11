b7de235984f70decfdb14bc313f9884ce60b8634
Jingle Huang <jinglhua@adobe.com>
2024-01-10 21:45:26 -0600

    wip

---
+++ express/blocks/pricing-cards/pricing-cards.js
@@ -0,0 +1,97 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable max-len */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable function-paren-newline */
+import { createTag } from '../../scripts/utils.js';
+
+const blockKeys = ['header', 'explain', 'offer', 'pricingContainer', 'ctaGroup', 'featureList', 'compare'];
+
+function decorateCard({ header, explain, offer, pricingContainer, ctaGroup, featureList, compare }) {
+  const card = createTag('div', { class: 'card' });
+
+  header.classList.add('card-header');
+  const h3 = header.querySelector('h3');
+  const h3Content = h3.textContent.trim();
+  const headerConfig = /\((.+)\)/.exec(h3Content);
+  if (headerConfig) {
+    const cfg = headerConfig[1];
+    h3.textContent = h3Content.replace(`(${cfg})`, '').trim();
+    if (/^\d/.test(cfg)) {
+      const headCntDiv = createTag('div', { class: 'head-cnt' });
+      headCntDiv.textContent = cfg;
+      header.append(headCntDiv);
+    } else {
+      const specialPromo = createTag('div');
+      specialPromo.textContent = cfg;
+      card.classList.add('special-promo');
+      card.append(specialPromo);
+    }
+  }
+
+  const icon = header.querySelector('img');
+  if (icon) {
+    if (icon.parentNode.tagName.toLowerCase() === 'p') {
+      icon.parentNode.remove();
+    }
+    h3.prepend(icon);
+  }
+  card.append(header);
+
+  if (explain.textContent.trim()) {
+    explain.classList.add('card-explain');
+    card.append(explain);
+  }
+
+  if (offer.textContent.trim()) {
+    offer.classList.add('card-offer');
+    pricingContainer.append(offer);
+  }
+
+  pricingContainer.classList.add('card-pricing');
+  card.append(pricingContainer);
+
+  ctaGroup.classList.add('card-cta-group');
+  ctaGroup.querySelectorAll('a').forEach((a, i) => {
+    a.classList.add('large');
+    if (i === 1) a.classList.add('secondary');
+    if (a.parentNode.tagName.toLowerCase() === 'em') {
+      a.classList.add('button', 'primary');
+      a.parentNode.remove();
+    }
+    if (a.parentNode.tagName.toLowerCase() === 'p') {
+      a.parentNode.remove();
+    }
+    card.append(a);
+  });
+  card.append(ctaGroup);
+
+  if (featureList.innerHTML.trim()) {
+    featureList.classList.add('card-feature-list');
+    card.append(featureList);
+  }
+
+  if (compare.innerHTML.trim()) {
+    compare.classList.add('card-compare');
+    compare.querySelector('a').classList.remove('button', 'accent');
+    card.append(compare);
+  }
+
+  return card;
+}
+
+export default function init(el) {
+  const divs = blockKeys.map((_, index) =>
+    el.querySelectorAll(`:scope > div:nth-child(${index + 1}) > div`),
+  );
+  const cards = Array.from(divs[0]).map((_, index) =>
+    blockKeys.reduce((obj, key, keyIndex) => {
+      obj[key] = divs[keyIndex][index];
+      return obj;
+    }, {}),
+  );
+  // const disclaimer = el.querySelector(':scope > div:nth-child(6)');
+  el.querySelectorAll(':scope > div').forEach((div) => div.remove());
+  const cardsContainer = createTag('div', { class: 'cards-container' });
+  cards.map((card) => decorateCard(card)).forEach((card) => cardsContainer.append(card));
+  el.append(cardsContainer);
+}
