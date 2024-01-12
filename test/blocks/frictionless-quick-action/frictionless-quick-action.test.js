accf81ba84165ad6d36c2316476a8ddbe3c5aa67
Victor Hargrave <vhargrave@adobe.com>
2024-01-12 10:00:07 +0100

    MWPW-140480 cleanup tests

--- test/blocks/frictionless-quick-action/frictionless-quick-action.test.js
+++ test/blocks/frictionless-quick-action/frictionless-quick-action.test.js
@@ -2,30 +2,7 @@ import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
 
 import init from '../../../express/blocks/frictionless-quick-action/frictionless-quick-action.js';
-import { setConfig } from '../../../express/scripts/utils.js';
 
-const locales = {
-  '': { ietf: 'en-US', tk: 'hah7vzn.css' },
-  br: { ietf: 'pt-BR', tk: 'inq1xob.css' },
-  cn: { ietf: 'zh-Hans-CN', tk: 'puu3xkp' },
-  de: { ietf: 'de-DE', tk: 'vin7zsi.css' },
-  dk: { ietf: 'da-DK', tk: 'aaz7dvd.css' },
-  es: { ietf: 'es-ES', tk: 'oln4yqj.css' },
-  fi: { ietf: 'fi-FI', tk: 'aaz7dvd.css' },
-  fr: { ietf: 'fr-FR', tk: 'vrk5vyv.css' },
-  gb: { ietf: 'en-GB', tk: 'pps7abe.css' },
-  in: { ietf: 'en-GB', tk: 'pps7abe.css' },
-  it: { ietf: 'it-IT', tk: 'bbf5pok.css' },
-  jp: { ietf: 'ja-JP', tk: 'dvg6awq' },
-  kr: { ietf: 'ko-KR', tk: 'qjs5sfm' },
-  nl: { ietf: 'nl-NL', tk: 'cya6bri.css' },
-  no: { ietf: 'no-NO', tk: 'aaz7dvd.css' },
-  se: { ietf: 'sv-SE', tk: 'fpk1pcd.css' },
-  tw: { ietf: 'zh-Hant-TW', tk: 'jay0ecd' },
-  uk: { ietf: 'en-GB', tk: 'pps7abe.css' },
-};
-
-setConfig({ locales });
 document.body.innerHTML = await readFile({ path: './mocks/crop-image-quick-action.html' });
 
 describe('Frictionless Quick Action Block', () => {
