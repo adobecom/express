55bcea76372007a75a613d816165dab0f1afa0bb
Victor Hargrave <vhargrave@adobe.com>
2024-01-12 13:31:32 +0100

    MWPW-140075 merge stage

--- express/scripts/express-delayed.js
+++ express/scripts/express-delayed.js
@@ -1,19 +1,52 @@
-import { createTag } from './utils.js';
+import {
+  fetchPlaceholders,
+  createTag,
+  getMetadata,
+} from './utils.js';
 
-function loadExpressProduct() {
+export const loadExpressProduct = () => {
   if (!window.hlx.preload_product) return;
   if (document.body.dataset.device === 'mobile') return;
   const path = ['www.adobe.com'].includes(window.location.hostname)
     ? 'https://new.express.adobe.com/static/preload.html' : 'https://stage.projectx.corp.adobe.com/static/preload.html';
   const iframe = createTag('iframe', { src: path, style: 'display:none' });
   document.body.append(iframe);
+};
+
+async function shouldBringToProduct() {
+  const userProfile = window.adobeProfile?.getUserProfile();
+  const placeholders = await fetchPlaceholders();
+  const autoRedirect = ['yes', 'true', 'Y', 'on'].includes(getMetadata('direct-path-to-product'));
+  const autoRedirectLanguageFound = placeholders.cancel && placeholders['bmtp-header'] && placeholders['bmtp-cancel-text'];
+  const isDesktop = document.body.dataset.device === 'desktop';
+
+  return userProfile && autoRedirect && autoRedirectLanguageFound && isDesktop;
 }
 
-export default function loadDelayed(DELAY = 7000) {
+const PEP_DELAY = 3000;
+
+/**
+ * Executes everything that happens a lot later, without impacting the user experience.
+ */
+export default async function loadDelayed(DELAY = 3000) {
+  const bringMeToProduct = await shouldBringToProduct();
+  if (bringMeToProduct) {
+    const { default: loadLoginUserAutoRedirect } = await import('../features/direct-path-to-product/direct-path-to-product.js');
+    return new Promise((resolve) => {
+      // TODO: not preloading product this early to protect desktop CWV
+      // until we see significant proof of preloading improving product load time
+      // loadExpressProduct();
+      setTimeout(() => {
+        loadLoginUserAutoRedirect();
+        resolve();
+      }, PEP_DELAY);
+    });
+  }
+
   return new Promise((resolve) => {
     setTimeout(() => {
       loadExpressProduct();
       resolve();
-    }, DELAY);
+    }, window.delay_preload_product ? DELAY * 2 : DELAY);
   });
 }
