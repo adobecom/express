44129df41043fa90692899b76c654388e0c6790e
Victor Hargrave <vhargrave@adobe.com>
2024-01-15 15:30:46 +0100

    MWPW-137051 test not blocking thread

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -8,6 +8,7 @@ import {
   setConfig,
   loadStyle,
   createTag,
+  getConfig,
 } from './utils.js';
 
 const locales = {
@@ -87,11 +88,14 @@ const showNotifications = () => {
 (async function loadPage() {
   if (window.hlx.init || window.isTestEnv) return;
   setConfig(config);
+
   if (getMetadata('hide-breadcrumbs') !== 'true' && !getMetadata('breadcrumbs') && !window.location.pathname.endsWith('/express/')) {
     const meta = createTag('meta', { name: 'breadcrumbs', content: 'on' });
     document.head.append(meta);
-  }
-  if (getMetadata('breadcrumbs') === 'on' && (!!getMetadata('short-title') || !!getMetadata('breadcrumbs-page-title'))) document.body.classList.add('breadcrumbs-spacing');
+    import('./gnav.js').then((gnav) => gnav.buildBreadCrumbArray(getConfig().locale.prefix.replaceAll('/', ''))).then((breadcrumbs) => {
+      if (breadcrumbs && breadcrumbs.length) document.body.classList.add('breadcrumbs-spacing');
+    });
+  } else if (getMetadata('breadcrumbs') === 'on' && !!getMetadata('breadcrumbs-base') && (!!getMetadata('short-title') || !!getMetadata('breadcrumbs-page-title'))) document.body.classList.add('breadcrumbs-spacing');
   showNotifications();
   await loadArea();
   if (['yes', 'true', 'on'].includes(getMetadata('mobile-benchmark').toLowerCase()) && document.body.dataset.device === 'mobile') {
