06671d6e884bcd7c050fc2554b8b74f8c80dd667
Qiyun Dai <pianoquin@gmail.com>
2024-01-30 11:18:22 -0600

    refactored to support 3 variants

--- express/scripts/utils/free-plan.js
+++ express/scripts/utils/free-plan.js
@@ -6,48 +6,80 @@ import {
   lazyLoadLottiePlayer,
 } from '../utils.js';
 
-export async function buildStaticFreePlanWidget() {
+const typeMap = {
+  branded: [
+    'free-plan-check-1',
+    'free-plan-check-2',
+  ],
+  features: [
+    'free-plan-features-1',
+    'free-plan-check-2',
+  ],
+  entitled: [
+    'entitled-plan-tag',
+  ],
+};
+
+export async function buildFreePlanWidget(typeKey) {
   const placeholders = await fetchPlaceholders();
   const widget = createTag('div', { class: 'free-plan-widget' });
-  for (let i = 1; i < 3; i += 1) {
-    const tagText = placeholders[`free-plan-check-${i}`];
-    const textDiv = createTag('div');
-    textDiv.textContent = tagText;
-    widget.append(textDiv);
-  }
+
+  typeMap[typeKey].forEach((tagKey) => {
+    const tagText = placeholders[tagKey];
+
+    if (tagText) {
+      const textDiv = createTag('span', { class: 'plan-widget-tag' });
+      textDiv.textContent = tagText;
+      widget.append(textDiv);
+    }
+  });
+
   return widget;
 }
 
 export async function addFreePlanWidget(elem) {
-  if (elem && ['yes', 'true', 'y', 'on'].includes(getMetadata('show-free-plan').toLowerCase())) {
-    const placeholders = await fetchPlaceholders();
-    const widget = await buildStaticFreePlanWidget();
+  const freePlanMeta = getMetadata('show-free-plan').toLowerCase();
 
-    document.addEventListener('planscomparisonloaded', () => {
-      const $learnMoreButton = createTag('a', {
-        class: 'learn-more-button',
-        href: '#plans-comparison-container',
-      });
-      const lottieWrapper = createTag('span', { class: 'lottie-wrapper' });
+  if (!freePlanMeta || ['no', 'false', 'n', 'off'].includes(freePlanMeta)) return;
+  const placeholders = await fetchPlaceholders();
+  let widget;
 
-      $learnMoreButton.textContent = placeholders['learn-more'];
-      lottieWrapper.innerHTML = getLottie('purple-arrows', '/express/icons/purple-arrows.json');
-      $learnMoreButton.append(lottieWrapper);
-      lazyLoadLottiePlayer();
-      widget.append($learnMoreButton);
-
-      $learnMoreButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        // temporarily disabling smooth scroll for accurate location
-        const $html = document.querySelector('html');
-        $html.style.scrollBehavior = 'unset';
-        const $plansComparison = document.querySelector('.plans-comparison-container');
-        $plansComparison.scrollIntoView();
-        $html.style.removeProperty('scroll-behavior');
-      });
-    });
-
-    elem.append(widget);
-    elem.classList.add('free-plan-container');
+  if (elem && ['yes', 'true', 'y', 'on', 'branded'].includes(freePlanMeta)) {
+    widget = await buildFreePlanWidget('branded');
   }
+
+  if (elem && ['features'].includes(freePlanMeta)) {
+    widget = await buildFreePlanWidget('features');
+  }
+
+  if (elem && ['entitled'].includes(freePlanMeta)) {
+    widget = await buildFreePlanWidget('entitled');
+  }
+
+  document.addEventListener('planscomparisonloaded', () => {
+    const learnMoreButton = createTag('a', {
+      class: 'learn-more-button',
+      href: '#plans-comparison-container',
+    });
+    const lottieWrapper = createTag('span', { class: 'lottie-wrapper' });
+
+    learnMoreButton.textContent = placeholders['learn-more'];
+    lottieWrapper.innerHTML = getLottie('purple-arrows', '/express/icons/purple-arrows.json');
+    learnMoreButton.append(lottieWrapper);
+    lazyLoadLottiePlayer();
+    widget.append(learnMoreButton);
+
+    learnMoreButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      // temporarily disabling smooth scroll for accurate location
+      const html = document.querySelector('html');
+      html.style.scrollBehavior = 'unset';
+      const $plansComparison = document.querySelector('.plans-comparison-container');
+      $plansComparison.scrollIntoView();
+      html.style.removeProperty('scroll-behavior');
+    });
+  });
+
+  elem.append(widget);
+  elem.classList.add('free-plan-container');
 }
