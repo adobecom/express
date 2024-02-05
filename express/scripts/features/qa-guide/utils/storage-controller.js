f25c86be9c37cf4aaea08c11bac5ec54769551e8
Qiyun Dai <pianoquin@gmail.com>
2024-02-05 14:13:14 -0600

    QA tool assertion (#518)
    * added json QA details output
    
    * logging test succeeded
    
    * remove devmode
    
    * refactor
    
    * test qa story selector on branch
    
    * add missed resp
    
    * use localStorage instead
    
    * session host locking from now on
    
    * custom story loading finished
    
    * deprecate separate audience col
    
    * disable dev mode
    
    * added interaction disable toggle
    
    * remove header
    
    * fix typo
    
    * Initial assertion library done
    
    * remove console log
    
    * wrong assertion method safe guard
    
    * fix mobile QA Tool max-height
    
    * fixing protocol issue

---
+++ express/scripts/features/qa-guide/utils/storage-controller.js
@@ -0,0 +1,83 @@
+import { getDevice } from '../../../utils.js';
+
+export function populateSessionStorage(payload) {
+  let storagePackage = JSON.parse(sessionStorage.getItem('qa-record'));
+
+  if (!storagePackage) {
+    storagePackage = {
+      configs: {
+        host: window.location.host,
+        audience: getDevice(),
+      },
+    };
+  } else {
+    storagePackage.configs.host = window.location.host;
+    storagePackage.configs.audience = getDevice();
+  }
+
+  payload.forEach((page) => {
+    const pagePackage = {
+      checks: {},
+      note: '',
+    };
+
+    page.items.forEach((item) => {
+      pagePackage.checks[item.idx] = {
+        value: item.text,
+        result: 'pending',
+      };
+    });
+
+    storagePackage[page.link] = pagePackage;
+  });
+
+  sessionStorage.setItem('qa-record', JSON.stringify(storagePackage));
+}
+
+export function updateSessionStorageChecks(page, form) {
+  const allCheckboxes = form.querySelectorAll('input[type="checkbox"]');
+  const noteArea = form.querySelector('textarea');
+  const sessionRecord = JSON.parse(sessionStorage.getItem('qa-record'));
+  const pageRecord = sessionRecord[page.link];
+
+  allCheckboxes.forEach((checkbox) => {
+    const checkRecord = pageRecord.checks[checkbox.dataset.idx];
+    if (checkRecord) {
+      checkRecord.result = checkbox.checked;
+    }
+  });
+
+  if (noteArea?.value) {
+    pageRecord.note = noteArea.value;
+  }
+
+  sessionRecord[page.link] = pageRecord;
+
+  sessionStorage.setItem('qa-record', JSON.stringify(sessionRecord));
+}
+
+export function setQAConfig(key, val) {
+  const sessionRecord = JSON.parse(sessionStorage.getItem('qa-record'));
+
+  if (sessionRecord) {
+    sessionRecord.configs[key] = val;
+    sessionStorage.setItem('qa-record', JSON.stringify(sessionRecord));
+  } else {
+    const configObj = {
+      configs: {},
+    };
+    configObj.configs[key] = val;
+
+    sessionStorage.setItem('qa-record', JSON.stringify(configObj));
+  }
+}
+
+export function getQAConfig(key) {
+  const sessionRecord = JSON.parse(sessionStorage.getItem('qa-record'));
+
+  if (sessionRecord) {
+    return sessionRecord.configs?.[key];
+  }
+
+  return null;
+}
