5da959a78e5504c3b4f5fded30f9d14c69c8cd65
Jingle Huang <jingleh321@gmail.com>
2024-02-06 13:00:14 -0800

    always preload product

--- express/scripts/express-delayed.js
+++ express/scripts/express-delayed.js
@@ -7,8 +7,7 @@ import {
 export const loadExpressProduct = () => {
   if (!window.hlx.preload_product) return;
   if (document.body.dataset.device === 'mobile') return;
-  const path = ['www.adobe.com'].includes(window.location.hostname)
-    ? 'https://new.express.adobe.com/static/preload.html' : 'https://stage.projectx.corp.adobe.com/static/preload.html';
+  const path = 'https://new.express.adobe.com/static/preload.html';
   const iframe = createTag('iframe', { src: path, style: 'display:none' });
   document.body.append(iframe);
 };
@@ -28,14 +27,14 @@ const PEP_DELAY = 3000;
 /**
  * Executes everything that happens a lot later, without impacting the user experience.
  */
-export default async function loadDelayed(DELAY = 15000) {
+export default async function loadDelayed(DELAY = 1000) {
   const bringMeToProduct = await shouldBringToProduct();
   if (bringMeToProduct) {
     const { default: loadLoginUserAutoRedirect } = await import('../features/direct-path-to-product/direct-path-to-product.js');
     return new Promise((resolve) => {
       // TODO: not preloading product this early to protect desktop CWV
       // until we see significant proof of preloading improving product load time
-      // loadExpressProduct();
+      loadExpressProduct();
       setTimeout(() => {
         loadLoginUserAutoRedirect();
         resolve();
