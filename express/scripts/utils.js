9334ff5c28db4a703dc8b4f1a991f1180ed7ffa1
Victor Hargrave <vhargrave@adobe.com>
2024-02-16 10:28:24 -0800

    MWPW-142867 prepare ims test

--- express/scripts/utils.js
+++ express/scripts/utils.js
@@ -1681,7 +1681,7 @@ export async function getExperimentConfig(experimentId) {
 
 function loadIMS() {
   window.adobeid = {
-    client_id: 'MarvelWeb3',
+    client_id: sessionStorage.getItem('imsclient'),
     scope: 'AdobeID,openid',
     locale: getConfig().locale.region,
     environment: 'prod',
