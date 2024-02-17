9f514e17c9547c0e5f4d5930716a0ec88d7cd45a
Eric S Chen <all42238@adobe.com>
2024-02-16 16:47:14 -0800

    Update marquee.js

--- express/blocks/marquee/marquee.js
+++ express/blocks/marquee/marquee.js
@@ -273,7 +273,7 @@ async function handleAnimation(div, typeHint, block, animations) {
     const srcURL = new URL(poster.src);
     const srcUSP = new URLSearchParams(srcURL.search);
     srcUSP.set('format', 'webply');
-    srcUSP.set('width', typeHint === 'mobile' ? 750 : 4080);
+    srcUSP.set('width', typeHint === 'mobile' ? 750 :  1024);
     optimizedPosterSrc = `${srcURL.pathname}?${srcUSP.toString()}`;
   }
 
@@ -283,8 +283,8 @@ async function handleAnimation(div, typeHint, block, animations) {
     title: (poster && poster.getAttribute('alt')) || '',
     params: videoParameters,
   };
-
   div.remove();
+  // div.style.display = "none"
 }
 
 async function handleContent(div, block, animations) {
@@ -372,8 +372,11 @@ export default async function decorate(block) {
   const animations = {};
   const rows = [...block.children];
   let index = 0
+
+
   
-  for await (const div of rows){
+  for (let index = 0; index < rows.length; index += 1) {
+    const div = rows[index];
    
     let rowType = 'animation';
     let typeHint; 
