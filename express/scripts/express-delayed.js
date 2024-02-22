9188e688256acc1e65d51ab7ad439ede25b1c1db
Jingle Huang <jingleh321@gmail.com>
2024-02-22 10:35:30 -0800

    PEP back to depend on IMS

--- express/scripts/express-delayed.js
+++ express/scripts/express-delayed.js
@@ -51,10 +51,7 @@ async function canPEP() {
   if (!placeholders.cancel || !placeholders['pep-header'] || !placeholders['pep-cancel']) return false;
   const segments = getSegmentsFromAlloyResponse(await window.alloyLoader);
   if (!pepSegment.replace(/\s/g, '').split(',').some((pepSeg) => segments.includes(pepSeg))) return false;
-  // eslint-disable-next-line no-unused-vars
-  const profileReady = !!(await isSignedIn()); // TODO: keeping for debugging adobeProfile
-  return true;
-  // return profileReady;
+  return !!(await isSignedIn());
 }
 
 const PEP_DELAY = 3000;
