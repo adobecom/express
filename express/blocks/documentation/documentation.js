c66e21d051b9d8e9aeaba4506a155155df4a44da
Eric S Chen <all42238@adobe.com>
2024-02-23 09:03:45 -0800

    Update documentation.js
    linted

--- express/blocks/documentation/documentation.js
+++ express/blocks/documentation/documentation.js
@@ -1,10 +1,10 @@
-// Block for displaying documentation. What this does is 
+// Block for displaying documentation. What this does is
 // display a table similar to the one in sharepoint for a list
-// of elements. This can be useful if you are documenting 
+// of elements. This can be useful if you are documenting
 // the structure of a block and choose to use a table to do so
 export default async function decorate(block) {
-    const children = block.querySelectorAll(':scope > div')
-    children.forEach((c) => {
-        c.classList.add("documentation-row")
-    })
+  const children = block.querySelectorAll(':scope > div');
+  children.forEach((c) => {
+    c.classList.add('documentation-row');
+  });
 }
