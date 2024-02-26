d62b62fe361f1d31a668ad58f9fbd0203aee65df
Eric S Chen <all42238@adobe.com>
2024-02-26 09:36:06 -0800

    Update pricing-cards.js

--- express/blocks/pricing-cards/pricing-cards.js
+++ express/blocks/pricing-cards/pricing-cards.js
@@ -233,12 +233,13 @@ const SALES_NUMBERS = '{{business-sales-numbers}}';
 export default async function init(el) {
   // For backwards compatability with old versions of the pricing card
   const newBlockVersion = el.querySelectorAll(':scope > div').length === 10;
+  const currentKeys = [...blockKeys];
   if (!newBlockVersion) {
-    blockKeys.splice(1, 1);
+    currentKeys.splice(1, 1);
   }
-  const divs = blockKeys.map((_, index) => el.querySelectorAll(`:scope > div:nth-child(${index + 1}) > div`));
+  const divs = currentKeys.map((_, index) => el.querySelectorAll(`:scope > div:nth-child(${index + 1}) > div`));
 
-  const cards = Array.from(divs[0]).map((_, index) => blockKeys.reduce((obj, key, keyIndex) => {
+  const cards = Array.from(divs[0]).map((_, index) => currentKeys.reduce((obj, key, keyIndex) => {
     obj[key] = divs[keyIndex][index];
     return obj;
   }, {}));
