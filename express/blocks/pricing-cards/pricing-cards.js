8e1e8f3b0141b532c884c8c3c3447252f360e2a6
Apoorva Ganapathy <apganapa@adobe.com>
2024-02-29 14:45:34 -0700

    removing unwanted change in the pricing-cards

--- express/blocks/pricing-cards/pricing-cards.js
+++ express/blocks/pricing-cards/pricing-cards.js
@@ -1,10 +1,6 @@
 import BlockMediator from '../../scripts/block-mediator.min.js';
 
-import {
-  createTag,
-  fetchPlaceholders,
-  setVisitorCountry,
-} from '../../scripts/utils.js';
+import { createTag, fetchPlaceholders } from '../../scripts/utils.js';
 import {
   buildUrl,
   formatSalesPhoneNumber,
@@ -302,7 +298,6 @@ function decorateCard({
 
 export default async function init(el) {
   // For backwards compatability with old versions of the pricing card
-  await setVisitorCountry();
   const legacyVersion = el.querySelectorAll(':scope > div').length < 10;
   const currentKeys = [...blockKeys];
   if (legacyVersion) {
