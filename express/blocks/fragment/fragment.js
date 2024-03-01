c4d7abc85fd31d52b96c59594d7e92ab2d42d9a5
Qiyun Dai <pianoquin@gmail.com>
2024-02-29 18:25:00 -0600

    new fragment logic

--- express/blocks/fragment/fragment.js
+++ express/blocks/fragment/fragment.js
@@ -1,14 +1,7 @@
-/* eslint-disable import/named, import/extensions */
-
-/*
- * Fragment Block
- * Include content from one Helix page in another.
- * https://www.hlx.live/developer/block-collection/fragment
- */
-
 import {
   decorateMain,
   loadSections,
+  removeIrrelevantSections,
 } from '../../scripts/utils.js';
 
 /**
@@ -22,8 +15,9 @@ async function loadFragment(path) {
     if (resp.ok) {
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
+      removeIrrelevantSections(main);
       const sections = await decorateMain(main);
-      await loadSections(sections);
+      await loadSections(sections, false);
       return main;
     }
   }
@@ -36,24 +30,25 @@ export default async function decorate(block) {
   const fragment = await loadFragment(path);
 
   if (fragment) {
-    const fragmentSection = fragment.querySelector(':scope .section');
+    const fragmentSections = fragment.querySelectorAll(':scope .section');
 
-    if (fragmentSection) {
-      const audience = fragmentSection.dataset?.audience;
-      if (audience) {
-        if (audience !== document.body.dataset?.device) {
-          block.closest('.section').remove();
-          return;
+    const blockSection = block.closest('.section');
+
+    if (blockSection) {
+      let currentNode = blockSection;
+      fragmentSections.forEach((fragmentSection, idx) => {
+        if (idx < 1) {
+          blockSection.classList.add(...fragmentSection.classList);
+          if (block.closest('.fragment-wrapper')) {
+            block.closest('.fragment-wrapper').replaceWith(...fragmentSection.childNodes);
+          } else {
+            block.replaceWith(...fragmentSection.childNodes);
+          }
+        } else {
+          currentNode.after(fragmentSection);
+          currentNode = fragmentSection;
         }
-        block.closest('.section').dataset.audience = audience;
-      }
-
-      block.closest('.section')?.classList.add(...fragmentSection.classList);
-      if (block.closest('.fragment-wrapper')) {
-        block.closest('.fragment-wrapper').replaceWith(...fragmentSection.childNodes);
-      } else {
-        block.replaceWith(...fragmentSection.childNodes);
-      }
+      });
     }
   }
 }
