f9235e5696caeafab0c98005626747240fcf4a74
Qiyun Dai <pianoquin@gmail.com>
2024-03-05 10:42:43 -0600

    Fix small pricing page visual + promo border suppression issues (#705)

--- express/blocks/pricing-cards/pricing-cards.js
+++ express/blocks/pricing-cards/pricing-cards.js
@@ -19,6 +19,7 @@ function suppressOfferEyebrow(specialPromo, legacyVersion) {
   if (specialPromo.parentElement) {
     if (legacyVersion) {
       specialPromo.parentElement.classList.remove('special-promo');
+      specialPromo.remove();
     } else {
       specialPromo.className = 'hide';
       specialPromo.parentElement.className = '';
@@ -83,9 +84,13 @@ function handlePrice(placeholders, pricingArea, placeholderArr, specialPromo, le
     if (specialPromo && !specialPromoPercentageEyeBrowTextReplaced) {
       const offerTextContent = specialPromo.textContent;
 
-      const shouldSuppress = shallSuppressOfferEyebrowText(response.savePer, offerTextContent,
-
-        isPremiumCard, true, response.offerId);
+      const shouldSuppress = shallSuppressOfferEyebrowText(
+        response.savePer,
+        offerTextContent,
+        isPremiumCard,
+        true,
+        response.offerId,
+      );
       if (shouldSuppress) {
         suppressOfferEyebrow(specialPromo, legacyVersion);
       } else {
