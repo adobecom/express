b98883821e033bd99a1bf04ddf24ea30b0e279ae
Jingle Huang <jingleh321@gmail.com>
2024-03-11 13:54:10 -0700

    billing-radio to button

--- express/blocks/billing-radio/billing-radio.js
+++ express/blocks/billing-radio/billing-radio.js
@@ -3,45 +3,32 @@ import { createTag } from '../../scripts/utils.js';
 import BlockMediator from '../../scripts/block-mediator.min.js';
 import { addTempWrapper } from '../../scripts/decorate.js';
 
-const getId = (function idSetups() {
-  const gen = (function* g() {
-    let id = 0;
-    while (true) {
-      yield id;
-      id += 1;
-    }
-  }());
-  return () => gen.next().value;
-}());
-
 const BILLING_PLAN = 'billing-plan';
 
 export default function init(el) {
   addTempWrapper(el, 'billing-radio');
 
-  const blockId = getId();
   const title = el.querySelector('strong');
   const plans = Array.from(el.querySelectorAll('ol > li')).map((li) => li.textContent.trim());
   el.innerHTML = '';
   el.append(title);
+  const buttons = [];
   plans.forEach((plan, planIndex) => {
-    const wrapper = createTag('div');
-    wrapper.addEventListener('change', () => {
+    const button = createTag('button', {
+      class: planIndex === 0 ? 'checked' : '',
+    }, plan);
+    button.prepend(createTag('span'));
+    button.addEventListener('click', () => {
+      if (planIndex === BlockMediator.get(BILLING_PLAN)) return;
       BlockMediator.set(BILLING_PLAN, planIndex);
     });
-    const label = createTag('label', {}, plan);
-    const radio = createTag('input', {
-      type: 'radio', name: `billing-${blockId}`, value: planIndex,
-    });
-    label.prepend(radio);
-    wrapper.append(label);
-    el.append(wrapper);
+    el.append(button);
+    buttons.push(button);
   });
-  el.querySelector('input[type="radio"]').checked = true;
 
   if (!BlockMediator.hasStore(BILLING_PLAN)) BlockMediator.set(BILLING_PLAN, 0);
   BlockMediator.subscribe(BILLING_PLAN, ({ newValue, oldValue }) => {
-    el.querySelector(`input[value="${oldValue}"]`).checked = false;
-    el.querySelector(`input[value="${newValue}"]`).checked = true;
+    buttons[oldValue].classList.remove('checked');
+    buttons[newValue].classList.add('checked');
   });
 }
--- test/unit/blocks/billing-radio/billing-radio.test.js
+++ test/unit/blocks/billing-radio/billing-radio.test.js
@@ -7,6 +7,8 @@ const { default: decorate } = await import(
 );
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 
+const isButtonChecked = (button) => button.classList.contains('checked');
+
 describe('Billing Radio', () => {
   let blocks;
   before(async () => {
@@ -18,31 +20,23 @@ describe('Billing Radio', () => {
   it('Billing Radio exists', () => {
     expect(blocks.every((block) => !!block)).to.be.true;
   });
-  it('Billing Radio uses incremental id when multiple on same page', () => {
-    expect(
-      blocks.every((block, i) => [...block.querySelectorAll('input[type=radio]')].every((radio) => radio.name === `billing-${i}`)),
-    ).to.be.true;
-  });
   it('Billing Radio element structure is correct', () => {
     blocks.forEach((block, i) => {
       const strong = block.querySelector('strong');
       expect(strong).to.exist;
-      const options = block.querySelectorAll(':scope > div');
+      const options = block.querySelectorAll(':scope > button');
       expect(options).to.have.lengthOf(i === 2 ? 4 : 2);
       options.forEach((option) => {
-        const input = option.querySelector('input');
-        expect(input).to.exist;
-        expect(input.type).to.equal('radio');
-        const label = option.querySelector('label');
-        expect(label).to.exist;
-        expect(label.textContent.trim()).to.not.be.empty;
+        const circle = option.querySelector('span');
+        expect(circle).to.exist;
+        expect(option.textContent.trim()).to.not.be.empty;
       });
     });
   });
   it('Billing Radio has 1st checked', () => {
     blocks.forEach((block) => {
-      block.querySelectorAll('input').forEach((input, index) => {
-        expect(input.checked).to.equal(index === 0);
+      block.querySelectorAll('button').forEach((button, index) => {
+        expect(isButtonChecked(button)).to.equal(index === 0);
       });
     });
   });
@@ -51,18 +45,18 @@ describe('Billing Radio', () => {
     expect(BlockMediator.get('billing-plan')).to.equal(0);
   });
   it('Billing Radio changes after clicking', () => {
-    blocks[0].querySelectorAll('input')[1].click();
+    blocks[0].querySelectorAll('button')[1].click();
     blocks.forEach((block) => {
-      block.querySelectorAll('input').forEach((input, index) => {
-        expect(input.checked).to.equal(index === 1);
+      block.querySelectorAll('button').forEach((button, index) => {
+        expect(isButtonChecked(button)).to.equal(index === 1);
       });
     });
     expect(BlockMediator.get('billing-plan')).to.equal(1);
 
-    blocks[2].querySelectorAll('input')[2].click();
+    blocks[2].querySelectorAll('button')[2].click();
     blocks.forEach((block) => {
-      block.querySelectorAll('input').forEach((input, index) => {
-        expect(input.checked).to.equal(index === 2);
+      block.querySelectorAll('button').forEach((button, index) => {
+        expect(isButtonChecked(button)).to.equal(index === 2);
       });
     });
     expect(BlockMediator.get('billing-plan')).to.equal(2);
@@ -73,11 +67,11 @@ describe('Billing Radio', () => {
       triggered += 1;
     });
     expect(triggered).to.equal(0);
-    blocks[0].querySelectorAll('input')[0].click();
+    blocks[0].querySelectorAll('button')[0].click();
     expect(triggered).to.equal(1);
-    blocks[1].querySelectorAll('input')[1].click();
+    blocks[1].querySelectorAll('button')[1].click();
     expect(triggered).to.equal(2);
-    blocks[0].querySelectorAll('input')[1].click();
+    blocks[0].querySelectorAll('button')[1].click();
     expect(triggered).to.equal(2);
     unsub();
   });
