6dac5e73962ca695392a9bd793dbf725403a0b8b
Eric S Chen <all42238@adobe.com>
2024-03-22 14:29:35 -0700

    Update floating-button.js

--- express/blocks/floating-button/floating-button.js
+++ express/blocks/floating-button/floating-button.js
@@ -25,7 +25,6 @@ export default async function decorate(block) {
     );
 
     const blockLinks = blockWrapper.querySelectorAll('a');
-
     if (blockLinks && blockLinks.length > 0) {
       formatDynamicCartLink(blockLinks[0]);
       const linksPopulated = new CustomEvent('linkspopulated', {
