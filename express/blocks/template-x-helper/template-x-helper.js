960e2b376227ab5fda31fd9e4118df31d9c80d7b
Jingle Huang <jingleh321@gmail.com>
2024-04-01 16:53:39 -0700

    wip

--- express/blocks/template-x-helper/template-x-helper.js
+++ express/blocks/template-x-helper/template-x-helper.js
@@ -2,6 +2,21 @@ import { createTag, loadScript } from '../../scripts/utils.js';
 import { html, render, useReducer } from '../../scripts/libs/htm-preact.js';
 import renderTemplateX from '../template-x/template-x.js';
 
+const initialState = {
+  text: 'where you can have some text',
+  blank_tepmlate: 'an image',
+  orientation: 'Vertical',
+  width: 'sixcols',
+  tasks: '',
+  topics: '',
+  collectionID: '',
+  limit: 10,
+  sort: 'Newest to Oldest',
+  locales: '',
+  animated: 'all',
+  premium: 'false',
+  q: '',
+};
 const reducer = {
 
 };
