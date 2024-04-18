c1ea71160603087e2d82b1b894ca8f022e4e6e07
Jingle Huang <jingleh321@gmail.com>
2024-04-18 09:27:59 -0500

    ut

--- express/blocks/pricing-cards/pricing-cards.js
+++ express/blocks/pricing-cards/pricing-cards.js
@@ -161,7 +161,7 @@ function readBraces(inputString, card) {
     const [token, promoType] = matches[matches.length - 1];
     const specialPromo = createTag('div');
     [specialPromo.textContent] = inputString.split(token);
-    card.classList.add(promoType);
+    card.classList.add(promoType.replaceAll(' ', ''));
     card.append(specialPromo);
     return specialPromo;
   }
