80996b73c30833e1a2acbec735e6150635b7294c
Jingle Huang <32369333+JingleH@users.noreply.github.com>
2024-04-29 08:08:26 -0700

    [Pricing] New Headline Block (#862)
    Added new timeline block

---
+++ test/unit/blocks/headline/headline.test.js
@@ -0,0 +1,45 @@
+import { readFile } from '@web/test-runner-commands';
+import { expect } from '@esm-bundle/chai';
+import { spy } from 'sinon';
+
+const { default: decorate } = await import('../../../../express/blocks/headline/headline.js');
+document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+describe('Headline', () => {
+  let oldLana;
+  let lanaSpy;
+  before(() => {
+    oldLana = window.lana;
+    window.lana = { log: () => {} };
+    lanaSpy = spy(window.lana, 'log');
+  });
+  after(() => {
+    window.lana = oldLana;
+  });
+  it('adds style to headline elements', () => {
+    for (let i = 1; i <= 3; i += 1) {
+      const headline = decorate(document.getElementById(`headline-ut-${i}`)).querySelector(`h${i}`);
+      expect(headline.style.fontSize).to.equal(`${i}rem`);
+      expect(headline.style.paddingTop).to.equal(`${i}rem`);
+      expect(headline.style.paddingBottom).to.equal(`${i}rem`);
+      expect(headline.parentElement.firstElementChild === headline.parentElement.lastElementChild);
+    }
+  });
+  it('works without config', () => {
+    const headline = decorate(document.getElementById('headline-ut-no-cfg')).querySelector('h4');
+    expect(headline).to.exist;
+    expect(headline.parentElement.firstElementChild)
+      .to.equal(headline.parentElement.firstElementChild);
+  });
+  it('leaves only headline when config fails', () => {
+    const headline = decorate(document.getElementById('headline-ut-bad-cfg')).querySelector('h5');
+    expect(headline.parentElement.firstElementChild)
+      .to.equal(headline.parentElement.firstElementChild);
+    expect(lanaSpy.calledOnce).to.be.true;
+    expect(lanaSpy.calledTwice).to.be.false;
+  });
+  it('leaves nothing when whole thing fails', () => {
+    const block = decorate(document.getElementById('headline-ut-bad-block'));
+    expect(block.innerHTML === '').to.be.true;
+    expect(lanaSpy.calledTwice).to.be.true;
+  });
+});
