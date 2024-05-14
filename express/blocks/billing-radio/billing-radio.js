594ed54acfcf415509852b8b5c78dba02b9aea29
Eric S Chen <all42238@adobe.com>
2024-05-14 12:03:09 -0700

    Update billing-radio.js

--- express/blocks/billing-radio/billing-radio.js
+++ express/blocks/billing-radio/billing-radio.js
@@ -1,4 +1,3 @@
-// fires 'billing-plan' BM and has global sync values when multiple on same page
 import { createTag } from '../../scripts/utils.js';
 import BlockMediator from '../../scripts/block-mediator.min.js';
 import { addTempWrapper } from '../../scripts/decorate.js';
@@ -11,25 +10,79 @@ export default function init(el) {
   const title = el.querySelector('strong');
   const plans = Array.from(el.querySelectorAll('ol > li')).map((li) => li.textContent.trim());
   el.innerHTML = '';
+  el.setAttribute('role', 'radiogroup');
+  el.setAttribute('aria-labelledby', 'radio-group-label');
   el.append(title);
+  const label = el.children[0];
+  label.setAttribute('id', "radio-group-label");
   const buttons = [];
   if (BlockMediator.get(BILLING_PLAN) === undefined) BlockMediator.set(BILLING_PLAN, 0);
   plans.forEach((plan, planIndex) => {
+    const checked = planIndex === (BlockMediator.get(BILLING_PLAN) || 0);
     const button = createTag('button', {
       id: plan,
-      class: planIndex === (BlockMediator.get(BILLING_PLAN) || 0) ? 'checked' : '',
+      class: checked ? 'checked' : '',
     });
+    button.setAttribute('aria-checked', checked ? true : false);
     button.append(createTag('label', { for: plan }, plan));
+    button.setAttribute('role', 'radio');
     button.prepend(createTag('span'));
     button.addEventListener('click', () => {
-      if (planIndex === BlockMediator.get(BILLING_PLAN)) return;
-      BlockMediator.set(BILLING_PLAN, planIndex);
+      if (planIndex !== BlockMediator.get(BILLING_PLAN)) {
+        BlockMediator.set(BILLING_PLAN, planIndex);
+      }
     });
+    if (planIndex > 0){
+      button.setAttribute('tabindex' ,-1)
+    }
     el.append(button);
     buttons.push(button);
   });
+
+  function focusNextButton(currentIndex) {
+    const nextIndex = (currentIndex + 1) % buttons.length;
+    buttons[nextIndex].focus();
+  }
+
+  function focusPreviousButton(currentIndex) {
+    const prevIndex = (currentIndex - 1 + buttons.length) % buttons.length;
+    buttons[prevIndex].focus();
+  }
+
+  el.addEventListener('keydown', (e) => {
+    if (!e.target.isEqualNode(document.activeElement)) return;
+    const currentIndex = buttons.indexOf(e.target);
+    switch (e.code) {
+      case 'ArrowLeft':
+      case 'ArrowUp':
+        e.preventDefault();
+        focusPreviousButton(currentIndex);
+        break;
+      case 'ArrowRight':
+      case 'ArrowDown':
+        e.preventDefault();
+        focusNextButton(currentIndex);
+        break;
+      case 'Enter':
+      case 'Space':
+        e.preventDefault();
+        BlockMediator.set(BILLING_PLAN, currentIndex);
+        break;
+      case 'Tab':
+        console.log(el, el.nextElementSibling)
+          el.nextElementSibling.focus()
+
+        break;
+      default:
+        break;
+    }
+  });
+
   BlockMediator.subscribe(BILLING_PLAN, ({ newValue, oldValue }) => {
     buttons[oldValue || 0].classList.remove('checked');
+    buttons[oldValue || 0].setAttribute('aria-checked', 'false');
     buttons[newValue].classList.add('checked');
+    buttons[newValue].setAttribute('aria-checked', 'true');
+    buttons[newValue].focus();
   });
 }
