9527df0fd64dc24a7d3d2e95d13bdc50b25a18a0
Eric S Chen <all42238@adobe.com>
2024-05-14 08:46:50 -0700

    Update wayfinder.js

--- express/blocks/wayfinder/wayfinder.js
+++ express/blocks/wayfinder/wayfinder.js
@@ -1,8 +1,4 @@
-// fires 'billing-plan' BM and has global sync values when multiple on same page
-import { addTempWrapper } from '../../scripts/decorate.js';
-
 export default function decorate(el) {
-  addTempWrapper(el, 'billing-radio');
   const rows = el.querySelectorAll(':scope > div');
   rows[0].classList.add('text-row');
   rows[1].classList.add('cta-row');
