0f603e054ac08d9e3730fcf2ed26f5925b3c00d4
Jingle Huang <jingleh321@gmail.com>
2024-06-06 14:41:13 -0700

    fix double encoding

--- express/blocks/susi-light/susi-light.js
+++ express/blocks/susi-light/susi-light.js
@@ -4,34 +4,32 @@ import {
   createTag,
   loadScript,
   getConfig,
-  // getMetadata,
 } from '../../scripts/utils.js';
 
 const authParams = {
   dt: false,
   locale: getConfig().locale.ietf.toLowerCase(),
   response_type: 'code', // FIXME: to be finalized
-  // client_id: 'AdobeExpressWeb',
-  client_id: 'sentry-test-edu',
+  client_id: 'AdobeExpressWeb',
   scope: 'AdobeID,openid',
 };
 const config = {
   consentProfile: 'free', // FIXME: to be finalized
 };
-// const isPopup = true; // FIXME: to be finalized
 const variant = 'edu-express';
 const isStage = getConfig().env.name !== 'prod';
 const CDN_URL = `https://auth-light.identity${isStage ? '-stage' : ''}.adobe.com/sentry/wrapper.js`;
 const onRedirect = (e) => {
-  console.log('on redirect', e, e.detail);
-  console.log('redirecting to: ', e.detail);
+  // eslint-disable-next-line no-console
+  console.log('on redirect to', e.detail);
   window.location.assign(e.detail);
 };
 const onToken = (e) => {
-  console.log('on token', e);
+  // eslint-disable-next-line no-console
+  console.log('susi on token', e); // FIXME: to be removed
 };
 const onError = (e) => {
-  console.log('on error:', e);
+  window.lana?.log('on error:', e);
 };
 
 export default async function init(el) {
@@ -40,10 +38,9 @@ export default async function init(el) {
   await loadScript(CDN_URL);
   const susi = createTag('susi-sentry-light');
   susi.authParams = authParams;
-  susi.authParams.redirect_uri = redirectUri ? encodeURIComponent(redirectUri) : 'https://new.express.adobe.com/';
+  susi.authParams.redirect_uri = redirectUri ?? 'https://new.express.adobe.com/';
   susi.config = config;
   if (isStage) susi.stage = 'true';
-  // susi.popup = 'true';
   susi.variant = variant;
   susi.addEventListener('redirect', onRedirect);
   susi.addEventListener('on-token', onToken);
