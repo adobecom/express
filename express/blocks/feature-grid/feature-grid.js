49eb11ea8d4bc0da3929c896ed2abd31884b89eb
echen-adobe <all42238@adobe.com>
2024-07-08 13:29:50 -0700

    unrestricted setting of CSS properties available, to be cut down later

--- express/blocks/feature-grid/feature-grid.js
+++ express/blocks/feature-grid/feature-grid.js
@@ -44,40 +44,86 @@ function renderGridNode({
   return gridItem;
 }
 
-const decorateLoadMoreSection = (block, loadMoreInfo) => {
+const applyProperties = (block, properties) => { 
+  for (let key of Object.keys(properties)) {
+    const elements = block.querySelectorAll(key)
+    if (elements.length === 0) continue; 
+    for (let element of elements){
+      const scopeProperties = properties[key]
+      let style = ''
+      for (let styleKey of Object.keys(scopeProperties)){
+        if (styleKey === 'textContent'){
+          element.textContent = scopeProperties.textContent 
+        } else {
+          style += styleKey + ":" + scopeProperties[styleKey] + " "
+        }
+      } 
+      element.style = style
+    }
+    
+  }
+}
+
+const extractProperties = (block) => {
+  let rows = Array.from(block.querySelectorAll(':scope > div')) 
+  rows = rows.filter((row) =>  {
+    
+    return row?.children?.length === 2 && row?.children[0]?.textContent.includes("Properties") 
+  })
+  let allProperties = {} 
+  for (let propertyRow of rows){
+    const key = propertyRow.children[0].textContent
+    let properties; 
+    if (propertyRow.children[1].children.length === 0) {
+      properties = [propertyRow.children[1]] 
+    } else {
+      properties = Array.from(propertyRow.children[1].querySelectorAll('p'))
+    }
+    let scopeProperties = {}
+    for (let p of properties){
+      const [k,v] = p.textContent.split(":")
+      scopeProperties[k] = v
+    }
+    let selector = ":scope "
+    let possibleKeySelector = key.split("Properties:")
+    if (possibleKeySelector.length === 2)
+    selector += possibleKeySelector[1]
+    allProperties[selector] = scopeProperties
+    block.removeChild(propertyRow)
+  } 
+  return allProperties
+}
+
+const decorateLoadMoreSection = (block) => {
   const loadMoreWrapper = createTag('div', { class: 'load-more-div' });
   const loadMoreButton = createTag('button', { class: 'load-more-button' });
   const loadMoreText = createTag('span', { class: 'load-more-text' });
   const toggleChev = createTag('div', { class: 'load-more-chev' });
-
-  [loadMoreText.textContent] = loadMoreInfo.text;
-  loadMoreButton.append(loadMoreText, toggleChev);
+ 
+  loadMoreButton.append("Load More", toggleChev); 
   loadMoreWrapper.append(loadMoreButton);
-  block.append(loadMoreWrapper);
-
+  block.append(loadMoreWrapper); 
   loadMoreButton.addEventListener('click', () => {
     block.classList.toggle('expanded');
     if (block.classList.contains('expanded')) {
-      [, loadMoreText.textContent] = loadMoreInfo.text;
+      [, loadMoreText.textContent] ="Load More"
     } else {
-      [loadMoreText.textContent] = loadMoreInfo.text;
+      [loadMoreText.textContent] ="Load More"
     }
   });
 };
-
-function getLoadMoreText(rows) {
-  const loadMoreText = rows.pop().textContent.split('|').map((item) => item.trim());
-  const loadMore = { text: loadMoreText };
-  return loadMore;
-}
+ 
 
 export default function decorate(block) {
+  const properties = extractProperties(block)
+
   const inputRows = block.querySelectorAll(':scope > div > div');
+
   block.innerHTML = '';
   const rows = Array.from(inputRows);
-  const heading = rows.shift();
-  const loadMoreSection = rows.length > 4 ? getLoadMoreText(rows) : null;
-  const gridProps = rows.map((row) => {
+  const heading = rows.shift(); 
+  
+  const gridProps = rows.map((row) => { 
     const subText = row.querySelector('p');
     const media = row.querySelector('p:last-of-type > a, p:last-of-type > picture');
     const title = row.querySelector('h2');
@@ -106,7 +152,9 @@ export default function decorate(block) {
 
   block.append(heading, gridContainer);
 
-  if (gridProps.length > 4) {
-    decorateLoadMoreSection(block, loadMoreSection);
-  }
+ // if (gridProps.length > 4) {
+    decorateLoadMoreSection(block);
+ // }
+
+ applyProperties(block, properties)
 }
