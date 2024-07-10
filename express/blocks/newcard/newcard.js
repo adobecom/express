1bdeb97000a321cbd9c5fdd529ec8ef0110c1e47
Kenneth Lum <all00866@adobe.com>
2024-07-10 07:38:44 -0700

    debug

--- express/blocks/newcard/newcard.js
+++ express/blocks/newcard/newcard.js
@@ -5,6 +5,7 @@ import { createTag } from '../../scripts/utils.js';
  * @param {HTMLDivElement} $block
  */
 export default function decorate($block) {
+  console.log("NEW CARD");
   $block.querySelectorAll(':scope>div').forEach(($card) => {
     $card.classList.add('card');
     const $cardDivs = [...$card.children];
