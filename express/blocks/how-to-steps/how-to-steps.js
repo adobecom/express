901fe7274a25e6843177afc57804ffa5ad488121
echen-adobe <all42238@adobe.com>
2024-07-10 13:12:43 -0700

    How to updates
    Updates the how to block with the following upgrades.
    
    1) allows a variant "inline-tip" that will inline the number and the tip title instead of having them separate
    
    2) allows user to specify which number the tip will begin on.

--- express/blocks/how-to-steps/how-to-steps.js
+++ express/blocks/how-to-steps/how-to-steps.js
@@ -1,55 +1,74 @@
-/* eslint-disable import/named, import/extensions */
-
-import {
-  createTag,
-// eslint-disable-next-line import/no-unresolved
-} from '../../scripts/utils.js';
+import { createTag } from '../../scripts/utils.js';
 
 export default function decorate($block, name, doc) {
   const $howto = $block;
   const $heading = $howto.closest('.section').querySelector('h2, h3, h4');
   const $rows = Array.from($howto.children);
-
   const includeSchema = !$block.classList.contains('noschema');
+  const isInlineTip = $block.classList.contains('inline-tip');
 
-  const schema = {
-    '@context': 'http://schema.org',
-    '@type': 'HowTo',
-    name: ($heading && $heading.textContent.trim()) || document.title,
-    step: [],
-  };
+  const schema = createSchema($heading, document.title);
+  const startNumber = getStartNumber($block);
 
   $rows.forEach(($row, i) => {
     const $cells = Array.from($row.children);
-    schema.step.push({
-      '@type': 'HowToStep',
-      position: i + 1,
-      name: $cells[0].textContent.trim(),
-      itemListElement: {
-        '@type': 'HowToDirection',
-        text: $cells[1].textContent.trim(),
-      },
-    });
-    const $h3 = createTag('h3');
-    $h3.innerHTML = $cells[0].textContent.trim();
-    const $p = createTag('p');
-    $p.innerHTML = $cells[1].innerHTML;
-    const $text = createTag('div', { class: 'tip-text' });
-    $text.append($h3);
-    $text.append($p);
-    const $number = createTag('div', { class: 'tip-number' });
-    $number.innerHTML = `<span>${i + 1}</span>`;
-    $cells[0].remove();
-    $cells[1].innerHTML = '';
-    $cells[1].classList.add('tip');
-    $cells[1].append($number);
-    $cells[1].append($text);
+    updateSchema(schema, $cells, i);
+    decorateRow($cells, i, startNumber, isInlineTip);
   });
 
   if (includeSchema) {
-    const $schema = createTag('script', { type: 'application/ld+json' });
-    $schema.innerHTML = JSON.stringify(schema);
-    const $head = doc.head;
-    $head.append($schema);
+    appendSchema(schema, doc);
   }
+}
+
+function createSchema(heading, documentTitle) {
+  return {
+    '@context': 'http://schema.org',
+    '@type': 'HowTo',
+    name: (heading && heading.textContent.trim()) || documentTitle,
+    step: [],
+  };
+}
+
+function getStartNumber($block) {
+  const numberClass = Array.from($block.classList).find(className => parseInt(className) > 0);
+  return numberClass ? parseInt(numberClass) : 0;
+}
+
+function updateSchema(schema, $cells, index) {
+  schema.step.push({
+    '@type': 'HowToStep',
+    position: index + 1,
+    name: $cells[0].textContent.trim(),
+    itemListElement: {
+      '@type': 'HowToDirection',
+      text: $cells[1].textContent.trim(),
+    },
+  });
+}
+
+function decorateRow($cells, index, startNumber, isInlineTip) {
+  const $h3 = createTag('h3', {}, $cells[0].textContent.trim());
+  const $p = createTag('p', {}, $cells[1].innerHTML);
+  const $text = createTag('div', { class: 'tip-text' }, [$p]);
+  const $number = createTag('div', { class: 'tip-number' }, `<span>${index + 1 + startNumber}</span>`);
+
+  if (isInlineTip) {
+    const $numberWrapper = createTag('div', { class: 'inline-tip-wrapper' }, [$number, $h3]);
+    $cells[1].innerHTML = '';
+    $cells[1].classList.add('tip');
+    $cells[1].append($numberWrapper, $text);
+  } else {
+    $text.prepend($h3);
+    $cells[1].innerHTML = '';
+    $cells[1].classList.add('tip');
+    $cells[1].append($number, $text);
+  }
+
+  $cells[0].remove();
+}
+
+function appendSchema(schema, doc) {
+  const $schema = createTag('script', { type: 'application/ld+json' }, JSON.stringify(schema));
+  doc.head.append($schema);
 }
