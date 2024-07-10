07290b1f46a4d12de708b17cf444bd6ba311f44c
Victor Hargrave <vhargrave@adobe.com>
2024-07-10 16:01:29 +0200

    MWPW-141918 make template call the same as milo and refactor .hero lcp

--- express/scripts/decorate.js
+++ express/scripts/decorate.js
@@ -6,3 +6,29 @@ export function addTempWrapper($block, blockName) {
   parent.insertBefore(wrapper, $block);
   wrapper.append($block);
 }
+
+export function decorateHeroSection(h1) {
+  const heroPicture = h1.parentElement.querySelector('picture');
+  let heroSection;
+  const main = document.querySelector('main');
+  if (main.children.length === 1) {
+    heroSection = document.createElement('div');
+    heroSection.id = 'hero';
+    const div = document.createElement('div');
+    heroSection.append(div);
+    if (heroPicture) {
+      div.append(heroPicture);
+    }
+    div.append(h1);
+    main.prepend(heroSection);
+  } else {
+    heroSection = h1.closest('main > div');
+    heroSection.id = 'hero';
+    heroSection.removeAttribute('style');
+  }
+  if (heroPicture) {
+    heroPicture.classList.add('hero-bg');
+  } else {
+    heroSection.classList.add('hero-noimage');
+  }
+}
