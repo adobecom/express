06bc70e30710e501521f031f36ba1ac6ad3d78f6
echen-adobe <all42238@adobe.com>
2024-07-15 12:58:18 -0700

    initial

---
+++ express/blocks/sidemount-navbar/sidemount-navbar.js
@@ -0,0 +1,54 @@
+import { getIconElement } from "../../scripts/utils.js";
+
+const OFFSET_HEIGHT = 1000
+const MEDIAN_HEIGHT = 1300
+const FINAL_TOP_VALUE = 40
+
+function addSectionIDs (block) {
+    let i = 1;
+    for (let section of Array.from(document.querySelectorAll(".section"))){
+        
+        section.setAttribute('id',`section${i}`) 
+        i += 1
+    }
+}
+
+function removeButtonStyles (block) {
+    for (let button of Array.from(block.querySelectorAll('a'))){
+        button.classList.remove('button')
+        button.classList.remove('accent')
+    }
+}
+
+function decorateHeader (block) {
+    const header = Array.from(block.querySelectorAll(":scope > div"))[0]
+    const iconHTML = getLottie('arrow-down', '/express/icons/arrow-down.json');
+    header.innerHTML = iconHTML + header.innerHTML
+}
+
+export default function decorate(block) {
+
+    function checkScroll() {
+        const diff = MEDIAN_HEIGHT - OFFSET_HEIGHT
+        const scrollPosition = window.pageYOffset; 
+        if (scrollPosition >= MEDIAN_HEIGHT) {
+            block.style.top = FINAL_TOP_VALUE + "%"
+        }  else  if (scrollPosition   >= OFFSET_HEIGHT) {
+
+            let top = FINAL_TOP_VALUE + 50 * ((MEDIAN_HEIGHT - scrollPosition ) / diff)
+            block.style = "display:visible"
+            block.style.top = top + "%"
+        } else { 
+            block.style.display = 'none';
+        }
+    }
+
+    // Add scroll event listener
+    window.addEventListener('scroll', checkScroll);
+
+    // Initial check
+    checkScroll();
+    addSectionIDs(block)
+    removeButtonStyles(block)
+    decorateHeader(block)
+}
