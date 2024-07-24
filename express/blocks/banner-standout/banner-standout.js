cb5c74b1cbcf1c271c321e6d3e74a21a5a351c4c
Kenneth Lum <all00866@adobe.com>
2024-07-24 06:54:47 -0700

    add banner-standout

---
+++ express/blocks/banner-standout/banner-standout.js
@@ -0,0 +1,30 @@
+import { normalizeHeadings } from '../../scripts/utils.js';
+
+export default async function decorate(block) {
+  // normalizeHeadings(block, ['h2', 'h3']);
+  const buttons = block.querySelectorAll('a.button');
+  if (buttons.length > 1) {
+    block.classList.add('multi-button');
+  }
+  // button on dark background
+  buttons.forEach(($button) => {
+    $button.classList.remove('primary');
+    $button.classList.remove('secondary');
+
+    if (block.classList.contains('light')) {
+      $button.classList.remove('accent');
+      $button.classList.add('large', 'primary', 'reverse');
+    } else {
+      $button.classList.add('accent', 'dark');
+      if (block.classList.contains('multi-button')) {
+        $button.classList.add('reverse');
+      }
+    }
+  });
+
+  const phoneNumberTags = block.querySelectorAll('a[title="{{business-sales-numbers}}"]');
+  if (phoneNumberTags.length > 0) {
+    const { formatSalesPhoneNumber } = await import('../../scripts/utils/pricing.js');
+    await formatSalesPhoneNumber(phoneNumberTags);
+  }
+}
