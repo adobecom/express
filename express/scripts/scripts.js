7f06bba65afc63062a1fd2c964afc52c947f892a
Jackson Sandland <all48775@adobe.com>
2024-07-24 15:36:08 -0700

    adds georouting to express

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -11,18 +11,18 @@ import {
   getConfig,
 } from './utils.js';
 
-// ID * Launching Aug 22www.adobe.com/id/express
-
 const locales = {
   '': { ietf: 'en-US', tk: 'jdq5hay.css' },
   br: { ietf: 'pt-BR', tk: 'inq1xob.css' },
   cn: { ietf: 'zh-Hans-CN', tk: 'puu3xkp' },
   de: { ietf: 'de-DE', tk: 'vin7zsi.css' },
   dk: { ietf: 'da-DK', tk: 'aaz7dvd.css' },
+  eg: { ietf: 'en-EG', tk: 'pps7abe.css' },
   es: { ietf: 'es-ES', tk: 'oln4yqj.css' },
   fi: { ietf: 'fi-FI', tk: 'aaz7dvd.css' },
   fr: { ietf: 'fr-FR', tk: 'vrk5vyv.css' },
   gb: { ietf: 'en-GB', tk: 'pps7abe.css' },
+  id: { ietf: 'id-ID', tk: '' },
   in: { ietf: 'en-IN', tk: 'pps7abe.css' },
   it: { ietf: 'it-IT', tk: 'bbf5pok.css' },
   jp: { ietf: 'ja-JP', tk: 'dvg6awq' },
@@ -30,10 +30,9 @@ const locales = {
   nl: { ietf: 'nl-NL', tk: 'cya6bri.css' },
   no: { ietf: 'no-NO', tk: 'aaz7dvd.css' },
   se: { ietf: 'sv-SE', tk: 'fpk1pcd.css' },
+  tr: { ietf: 'tr-TR', tk: 'ley8vds.css' },
   tw: { ietf: 'zh-Hant-TW', tk: 'jay0ecd' },
   uk: { ietf: 'en-GB', tk: 'pps7abe.css' },
-  tr: { ietf: 'tr-TR', tk: 'ley8vds.css' },
-  eg: { ietf: 'en-EG', tk: 'pps7abe.css' },
 };
 
 let jarvisImmediatelyVisible = false;
