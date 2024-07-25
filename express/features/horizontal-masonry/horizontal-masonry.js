c11fd42eb9c1a1a6cd61523643fe724d65b8caf1
echen-adobe <all42238@adobe.com>
2024-07-25 10:44:45 -0700

    Update horizontal-masonry.js

--- express/features/horizontal-masonry/horizontal-masonry.js
+++ express/features/horizontal-masonry/horizontal-masonry.js
@@ -1,6 +1,25 @@
 import { createTag, getIconElement } from '../../scripts/utils.js'
 
-function createEnticement (enticementDetail,enticementIcon, mode) { 
+
+const promptTokenRegex = new RegExp('(%7B%7B|{{)prompt-text(%7D%7D|}})');
+
+export const windowHelper = {
+  redirect: (url) => {
+    window.location.assign(url);
+  },
+};
+
+function handleGenAISubmit(form, link) {
+  console.log('----')
+  const input = form.querySelector('input');
+  if (input.value.trim() === '') return;
+  const genAILink = link.replace(promptTokenRegex, encodeURI(input.value).replaceAll(' ', '+'));
+  
+  console.log(genAILink)
+  // if (genAILink) windowHelper.redirect(genAILink);
+}
+
+function createEnticement (enticementDetail,enticementLink, mode) { 
   const enticementDiv = createTag('div', { class: 'enticement-container' });
   const svgImage = getIconElement('enticement-arrow',60)
   let arrowText = enticementDetail 
@@ -10,6 +29,7 @@ function createEnticement (enticementDetail,enticementIcon, mode) {
   const button = createTag('button', {class : 'generate-small-btn' }) 
   buttonContainer.append(  button)
   button.textContent = 'Generate'
+  button.addEventListener('click',() => handleGenAISubmit(enticementDiv, enticementLink ))
   enticementDiv.append(enticementText, svgImage, input, buttonContainer);
   if (mode === 'light') enticementText.classList.add('light');
   return enticementDiv;
@@ -24,11 +44,10 @@ export default async function setHorizontalMasonry(el) {
   const enticementElement = args[0].querySelector('a')
   const enticementMode = el.classList.contains('light') ? 'light' : 'dark'; 
   const enticementText = enticementElement.textContent.trim();
-  const enticementIcon = enticementElement.href; 
-  console.log(enticementText, enticementIcon, enticementElement)
+  const enticementLink  = enticementElement.href;
   args[0].remove()
 
-  el.querySelector('.interactive-container').appendChild(createEnticement(enticementText, enticementIcon, enticementMode))
+  el.querySelector('.interactive-container').appendChild(createEnticement(enticementText, enticementLink, enticementMode))
   for (let i = 1; i < args.length; i += 4) {
     let divider = args[i]
     divider.remove()
