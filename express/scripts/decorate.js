9b68a77b3e32d4e08ed542fcdebc6efbcb6162c5
echen-adobe <all42238@adobe.com>
2024-07-25 14:02:41 -0700

    Update decorate.js

--- express/scripts/decorate.js
+++ express/scripts/decorate.js
@@ -6,17 +6,3 @@ export function addTempWrapper($block, blockName) {
   parent.insertBefore(wrapper, $block);
   wrapper.append($block);
 }
-
-
-export function defineDeviceByScreenSize() {
-  const DESKTOP_SIZE = 1200;
-  const MOBILE_SIZE = 600;
-  const screenWidth = window.innerWidth;
-  if (screenWidth >= DESKTOP_SIZE) {
-    return 'DESKTOP';
-  }
-  if (screenWidth <= MOBILE_SIZE) {
-    return 'MOBILE';
-  }
-  return 'TABLET';
-} 
