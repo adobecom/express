208a9344f0ab1bf6db7a91093eb91aa6a44c64cf
Kenneth Lum <all00866@adobe.com>
2024-07-29 11:02:41 -0700

    code cleanup

--- express/blocks/banner/banner.js
+++ express/blocks/banner/banner.js
@@ -1,92 +1,25 @@
 import { normalizeHeadings, createTag } from '../../scripts/utils.js';
 
-
-// export default async function decorate0(block) {
-//   normalizeHeadings(block, ['h2', 'h3']);
-//   const buttons = block.querySelectorAll('a.button');
-//   if (buttons.length > 1) {
-//     block.classList.add('multi-button');
-//   }
-//   // button on dark background
-//   buttons.forEach(($button) => {
-//     $button.classList.remove('primary');
-//     $button.classList.remove('secondary');
-
-//     if (block.classList.contains('light')) {
-//       $button.classList.remove('accent');
-//       $button.classList.add('large', 'primary', 'reverse');
-//     } else {
-//       $button.classList.add('accent', 'dark');
-//       if (block.classList.contains('multi-button')) {
-//         $button.classList.add('reverse');
-//       }
-//     }
-//   });
-
 export default async function decorate(block) {
-
-  // try with class / variant (hihi1, hi2, standout3 hmm)
-
-
-  console.log("=== block.classList", block.classList)
   const isBannerLightVariant = block.classList.contains('light');
   const isBannerStandoutVariant = block.classList.contains('standout');
 
-
-  console.log("=== in banner, block is", block);
-
-  for (const child of block.children) {
-    console.log("====== CHILD", child, child.tagName);
-  }
-
-  console.log("=== in banner, Variant", {
-    isBannerLightVariant,
-    isBannerStandoutVariant
-  });
-
   if (isBannerStandoutVariant) {
-
-    console.log("DOING THE STANDOUT VARIANT")
-
-    // const standoutContainer = document.createElement('div');
     const standoutContainer = createTag('div', {
       class: "standout-container"
     });
 
-    // const standoutMessageContainer = createTag('div', {
-    //   class: "banner-standout-message-container"
-    // });
-
-    // standoutContainer.append(standoutContainer)
-
     for (const child of block.children) {
-      console.log("====== CHILD", child, child.tagName);
       standoutContainer.append(child)
     }
-
-    // block.replaceChildren();
-    // block.append(standoutContainer)
 
     block.replaceChildren(standoutContainer);
 
     const arrBoldText = [...standoutContainer.querySelectorAll(":scope strong")];
-    console.log("=== arrBoldText", arrBoldText)
-
-    const arrGlobalBoldText = document.querySelectorAll("strong");
-    console.log("=== arrGlobalBoldText", arrBoldText)
 
     for (const el of arrBoldText) {
       el.classList.add("standout-text")
     }
-    // standoutContainer.append(block);
-
-    // standoutContainer.innerHTML = "haha 1";
-    // block.append(standoutContainer)
-
-    // const firstChildOfBlock = block.
-
-    // block.replaceChild(standoutContainer)
-
   }
 
 
@@ -119,7 +52,4 @@ export default async function decorate(block) {
     const { formatSalesPhoneNumber } = await import('../../scripts/utils/pricing.js');
     await formatSalesPhoneNumber(phoneNumberTags);
   }
-
-  console.log("=== Before returning")
-  // return standoutContainer;
 }
