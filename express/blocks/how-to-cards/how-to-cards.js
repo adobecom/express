a83a98fe9b8be9344c554f497a70c8a7bd64f75e
Jingle Huang <jingleh321@gmail.com>
2024-07-29 22:38:56 -0700

    wip

--- express/blocks/how-to-cards/how-to-cards.js
+++ express/blocks/how-to-cards/how-to-cards.js
@@ -2,26 +2,7 @@ import {
   createTag,
 } from '../../scripts/utils.js';
 
-export default function decorate(block) {
-  const section = block.closest('.section');
-  const heading = section.querySelector('h2, h3, h4');
-
-  const includeSchema = block.classList.contains('schema');
-  const cards = [...block.querySelectorAll(':scope > div')];
-  const mediaQuery = window.matchMedia('(min-width: 900px)');
-  // if (mediaQuery.matches) {
-    
-  // }
-  cards.forEach((div, index) => {
-    div.classList.add('card');
-    const content = div.querySelector('div');
-    const tipNumber = createTag('div', { class: 'number' });
-    tipNumber.append(createTag('span', { class: 'number-txt' }, index + 1));
-    tipNumber.append(createTag('div', { class: 'number-bg' }));
-    content.prepend(tipNumber);
-    content.classList.add('content');
-  });
-
+export function addSchema(bl, heading) {
   const schema = {
     '@context': 'http://schema.org',
     '@type': 'HowTo',
@@ -29,7 +10,7 @@ export default function decorate(block) {
     step: [],
   };
 
-  block.querySelectorAll('.content').forEach((step, i) => {
+  bl.querySelectorAll('.content').forEach((step, i) => {
     const h = step.querySelector('h3, h4, h5, h6');
     const p = step.querySelector('p');
 
@@ -45,11 +26,74 @@ export default function decorate(block) {
       });
     }
   });
+  document.head.append(createTag('script', { type: 'application/ld+json' }, JSON.stringify(schema)));
+}
 
-  if (includeSchema) {
-    const $schema = createTag('script', { type: 'application/ld+json' });
-    $schema.innerHTML = JSON.stringify(schema);
-    const $head = document.head;
-    $head.append($schema);
+export function removePages(bl, cards) {}
+
+export function addPages(bl, cards) {
+  if (bl.querySelector('.page-control')) {
+    
+  }
+  const dots = [];
+  let curr = 0;
+  const cnt = cards.length;
+  const control = createTag('div', { class: 'page-control' });
+  const pageStatus = createTag('div', { class: 'page-status' });
+  const prev = createTag('div', { class: 'prev' }, 'p');
+  const next = createTag('div', { class: 'next' }, 'n');
+  const changePage = (target) => {
+    cards[curr].classList.add('hide');
+    cards[target].classList.remove('hide');
+    dots[curr].classList.remove('curr');
+    dots[target].classList.add('curr');
+    curr = target;
+  };
+  prev.addEventListener('click', () => {
+    changePage((curr - 1 + cnt) % cnt);
+  });
+  next.addEventListener('click', () => {
+    changePage((curr + 1) % cnt);
+  });
+  cards.forEach((card, i) => {
+    if (i > 0) card.classList.add('hide');
+    const dot = createTag('div', { class: `dot${i === 0 ? ' curr' : ''}` });
+    dots.push(dot);
+    pageStatus.append(dot);
+  });
+  control.append(pageStatus, prev, next);
+  bl.append(control);
+}
+
+export default function decorate(bl) {
+  const section = bl.closest('.section');
+  const heading = section.querySelector('h2, h3, h4');
+
+  const cards = [...bl.querySelectorAll(':scope > div')];
+
+  cards.forEach((div, index) => {
+    div.classList.add('card');
+    const content = div.querySelector('div');
+    const tipNumber = createTag('div', { class: 'number' });
+    tipNumber.append(
+      createTag('span', { class: 'number-txt' }, index + 1),
+      createTag('div', { class: 'number-bg' }),
+    );
+    content.prepend(tipNumber);
+    content.classList.add('content');
+  });
+
+  const mediaQuery = window.matchMedia('(min-width: 900px)');
+  // const cardCnt = cards.length;
+  const needsPage = true || mediaQuery.match;
+
+  if (needsPage) {
+    addPages(bl, cards);
+  }
+
+  if (bl.classList.contains('schema')) {
+    addSchema(bl, heading);
   }
 }
+
+decorate(document.querySelector('.how-to-cards'));
