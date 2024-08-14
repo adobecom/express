0d81dbc0a29233cc670f3f7ec80d987836291521
Jingle Huang <jingleh321@gmail.com>
2024-08-13 18:21:27 -0700

    adapt webpage_template

--- express/scripts/template-search-api-v3.js
+++ express/scripts/template-search-api-v3.js
@@ -363,7 +363,7 @@ function isValidBehaviors(behaviors) {
 export function isValidTemplate(template) {
   return !!(template.status === 'approved'
     && template.customLinks?.branchUrl
-    && template.pages?.[0]?.rendition?.image?.thumbnail?.componentId
+    && (template.assetType === 'Webpage_Template' || template.pages?.[0]?.rendition?.image?.thumbnail?.componentId)
     && template._links?.['http://ns.adobe.com/adobecloud/rel/rendition']?.href?.replace
     && template._links?.['http://ns.adobe.com/adobecloud/rel/component']?.href?.replace
     && isValidBehaviors(template.behaviors));
