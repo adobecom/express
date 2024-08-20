07f97dc995dbcf1e62ed28103c7a23aaa293b7b4
Kenneth Lum <all00866@adobe.com>
2024-08-20 11:49:36 -0700

    embedding Adobe TV / MPC

--- express/blocks/tutorials/tutorials.js
+++ express/blocks/tutorials/tutorials.js
@@ -68,4 +68,33 @@ export default function decorate($block) {
   addTempWrapper($block, 'tutorials');
 
   decorateTutorials($block);
+
+  const tryEl = createTag('div', { class: '', tabindex: 0 });
+  // tryEl.innerHTML = "<h1>Hello World</h1>";
+  tryEl.innerHTML = `<h1>Hello World</h1><iframe title="Adobe Video Publishing Cloud Player" width="640" height="360" src="https://images-tv.adobe.com/mpcv3/8112/2c506a6a-e54c-42fb-869d-3bba4b133d0a_1718031054.854x480at800_h264.mp4" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>`;
+
+  tryEl.innerHTML = `<h1>Hello World</h1>
+  <iframe title="Adobe Video Publishing Cloud Player"
+    width="640" height="360"
+    src="https://images-tv.adobe.com/mpcv3/8112/2c506a6a-e54c-42fb-869d-3bba4b133d0a_1718031054.854x480at800_h264.mp4"
+    frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no">
+  </iframe>
+  <div>
+    <video controls width="640" src="https://images-tv.adobe.com/mpcv3/8112/2c506a6a-e54c-42fb-869d-3bba4b133d0a_1718031054.854x480at800_h264.mp4"></video>
+  </div>
+  `;
+
+
+  // Inline video good URL - https://video.tv.adobe.com/v/332632
+  // Inline video bad URL - https://images-tv.adobe.com/mpcv3/1384/b50a7a5f-3010-4e7b-bdc1-2f315711e508_1618253276.854x480at800_h264.mp4
+  // https://images-tv.adobe.com/mpcv3/8112/2c506a6a-e54c-42fb-869d-3bba4b133d0a_1718031054.854x480at800_h264.mp4
+
+  // https://images-tv.adobe.com/mpcv3/8112/2c506a6a-e54c-42fb-869d-3bba4b133d0a_1718031054.854x480at800_h264.mp4
+  // https://video.tv.adobe.com/v/332632
+
+
+// <iframe title="Adobe Video Publishing Cloud Player" width="640" height="360" src="https://video.tv.adobe.com/v/346121/" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>
+
+
+  $block.append(tryEl);
 }
