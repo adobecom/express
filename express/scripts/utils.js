e43f8a421f248a6fe5043e57019e919dc1f2153a
echen-adobe <159481679+echen-adobe@users.noreply.github.com>
2024-08-20 15:16:08 -0700

    [MWPW-154904] Interactive Marquee (#1037)
    * initial
    
    * portted milo styles over
    
    * added masonry layout
    
    * added enticment
    
    * major css changes
    
    * enticement arrow
    
    * added prompt button susi
    
    * Create firefly-susi.js
    
    * Revert "Create firefly-susi.js"
    
    This reverts commit 0c1c66b06e72e1e44e7ebc9539ea84c5f6767468.
    
    * Revert "added prompt button susi"
    
    This reverts commit 1f6481acd3024e0fab8eaac03e5416d993bef6f3.
    
    * Update horizontal-masonry.js
    
    * refactored CSS
    
    * removed untested and unsupported use cases
    
    * removed unused files
    
    * removed unneeded css change
    
    * Update decorate.js
    
    * removed stray console
    
    * updated gradient
    
    * css revamp to support wide pics
    
    * added test coverage
    
    * lint
    
    * Update utils.js
    
    * Update utils.js
    
    * Update utils.js
    
    * Update utils.js
    
    * Update hero-animation.css
    
    * Update utils.js
    
    * Update utils.js
    
    * Update horizontal-masonry.css
    
    * Update horizontal-masonry.css
    
    * Update horizontal-masonry.css
    
    * Update horizontal-masonry.css
    
    * reorganized css
    
    * Update interactive-marquee.js
    
    * Update interactive-marquee.css
    
    * Update interactive-marquee.js
    
    * Update interactive-marquee.js
    
    * Update styles.css
    
    * Update styles.css
    
    * removed unneeded pricing button class
    
    * Update horizontal-masonry.css
    
    * Update interactive-marquee.js
    
    * Update styles.css
    
    * latest round
    
    fixed text overflow for prompts
    fixed border of gen ai input box
    fixed button radius
    adjusted marquee dimensions
    removed gradient table row
    
    * Update horizontal-masonry.css
    
    * Update interactive-marquee.js
    
    * Update interactive-marquee.js
    
    * ...
    
    * Update horizontal-masonry.css
    
    * Update horizontal-masonry.css
    
    * Update horizontal-masonry.js
    
    * Update horizontal-masonry.js
    
    * added new icon
    
    * Update horizontal-masonry.css
    
    * Update horizontal-masonry.css
    
    * Update horizontal-masonry.js
    
    * added correct prompt overlay for mobile
    
    * added localization placeholder support
    
    * Update horizontal-masonry.js
    
    * logo injection
    
    * lint
    
    * Update horizontal-masonry.js
    
    * tests fixed
    
    * Update horizontal-masonry.js
    
    * Update horizontal-masonry.js

--- express/scripts/utils.js
+++ express/scripts/utils.js
@@ -2726,3 +2726,33 @@ export function registerPerformanceLogger() {
     // no output
   }
 }
+
+export const [setLibs, getLibs] = (() => {
+  let libs;
+  return [
+    (prodLibs, force = false) => {
+      if (force) {
+        libs = prodLibs;
+        return libs;
+      }
+      const { hostname } = window.location;
+      if (!hostname.includes('hlx.page')
+        && !hostname.includes('hlx.live')
+        && !hostname.includes('localhost')) {
+        libs = prodLibs;
+        return libs;
+      }
+      const branch = new URLSearchParams(window.location.search).get('milolibs') || 'main';
+      if (branch === 'local') {
+        libs = 'http://localhost:6456/libs';
+        return libs;
+      }
+      if (branch.indexOf('--') > -1) {
+        libs = `https://${branch}.hlx.live/libs`;
+        return libs;
+      }
+      libs = `https://${branch}--milo--adobecom.hlx.live/libs`;
+      return libs;
+    }, () => libs,
+  ];
+})();
