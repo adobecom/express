b84560dc72dc9ac391fca2b34db898e7c512f20c
echen-adobe <all42238@adobe.com>
2024-08-27 11:33:17 -0700

    added sitemap script

--- test/helpers/sitemap.js
+++ test/helpers/sitemap.js
@@ -1,1 +1,37 @@
-console.log('abc')
+import fs from "fs"
+import fetch from "node-fetch"
+import dotenv from "dotenv"
+dotenv.config() 
+
+const urls = [
+    "https://www.adobe.com/express/sitemap.xml"
+]
+const apiKey = process.env.apiKey
+ 
+async function makeRequests() {
+    const baseURL = "https://admin.hlx.page/sitemap/adobecom/express/stage/"
+    for (const url of urls) {
+        try { 
+            let parsed_url = url.split("https://www.adobe.com/")[1] 
+            const response = await fetch(baseURL + parsed_url, {
+                method: 'POST', 
+                headers: {
+                    'authorization' : `token ${apiKey}` 
+                },
+            });
+
+            if (!response.ok) {
+                console.log(response)
+                console.error(`Error: ${url}`, response.statusText);
+                continue;
+            }
+
+            const data = await response.text();
+            console.log(`Success: ${url}`, data);
+        } catch (error) {
+            console.log(error) 
+        }
+    }
+}
+
+makeRequests();
