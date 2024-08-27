68e0e45428a2dad10fa9486317ff0d821d13b0e3
echen-adobe <all42238@adobe.com>
2024-08-27 14:03:21 -0700

    updated to work with github actions

--- test/helpers/sitemap.js
+++ test/helpers/sitemap.js
@@ -1,34 +1,46 @@
-import fs from "fs"
-import fetch from "node-fetch"
-import dotenv from "dotenv"
-dotenv.config() 
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const urls = [
     "https://www.adobe.com/express/sitemap.xml"
-]
-const apiKey = process.env.apiKey
- 
+];
+const apiKey = process.env['HELIX_API_KEY'];
+if (! apiKey) {
+    throw new Error("Invalid API Key: " + apiKey)
+}
 async function makeRequests() {
-    const baseURL = "https://admin.hlx.page/sitemap/adobecom/express/stage/"
+    const baseURL = "https://admin.hlx.page/sitemap/adobecom/express/stage/";
+    let failedURLs = []
     for (const url of urls) {
         try { 
-            let parsed_url = url.split("https://www.adobe.com/")[1] 
+            let parsed_url = url.split("https://www.adobe.com/")[1];
             const response = await fetch(baseURL + parsed_url, {
                 method: 'POST', 
                 headers: {
-                    'authorization' : `token ${apiKey}` 
+                    'authorization': `token ${apiKey}`
                 },
             });
-            if (!response.ok) {
-                console.log(response)
+            if (response.status !== 200) {
                 console.error(`Error: ${url}`, response.statusText);
+                failedURLs.push(url)
                 continue;
             }
             const data = await response.text();
             console.log(`Success: ${url}`, data);
         } catch (error) {
-            console.log(error) 
+            console.log(error);
+            failedURLs.push(url)
         }
+    }
+    if (failedURLs.length > 0){
+        console.error('------------')
+        console.error("Failed to build some urls")
+        for (let url of failedURLs){
+            console.error(url)
+        }
+        console.error('------------')
+        throw new Error("Failed to rebuild some urls")
     }
 }
 
