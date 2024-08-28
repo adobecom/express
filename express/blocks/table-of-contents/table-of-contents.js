3f8d72111c89f0968e891d2f69c06162c4d4d59f
Jackson Sandland <all48775@adobe.com>
2024-08-28 16:18:12 -0700

    increase header offset

--- express/blocks/table-of-contents/table-of-contents.js
+++ express/blocks/table-of-contents/table-of-contents.js
@@ -59,7 +59,7 @@ function addTOCItemClickEvent(tocItem, heading, verticalLine) {
     tocItem.classList.add('active');
     verticalLine.style.display = 'block';
 
-    const headerOffset = 10;
+    const headerOffset = 65;
     const rect = heading.getBoundingClientRect();
     const offsetPosition = rect.top + window.scrollY - headerOffset;
 
