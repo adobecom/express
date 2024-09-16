094e486b2382e3feceae9de9ae790e43692db747
echen-adobe <all42238@adobe.com>
2024-09-16 10:08:43 -0700

    lint

--- express/blocks/logo-row/logo-row.js
+++ express/blocks/logo-row/logo-row.js
@@ -13,8 +13,8 @@ export default function decorate(block) {
   // Style image column
   imageColumn.classList.add('image-column');
 
-  const images = imageColumn.querySelectorAll('img')
+  const images = imageColumn.querySelectorAll('img');
   // Add classes to all images
   images.forEach((img) => img.classList.add('brand-image'));
-  if (images.length > 5) block.classList.add('numerous')
+  if (images.length > 5) block.classList.add('numerous');
 }
