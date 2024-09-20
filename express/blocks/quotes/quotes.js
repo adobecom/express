ba9bc148ab6fb5308dba33c1c3a8f7379dd10d43
Kenneth Lum <all00866@adobe.com>
2024-09-20 07:32:25 -0700

    code cleanup

--- express/blocks/quotes/quotes.js
+++ express/blocks/quotes/quotes.js
@@ -1,27 +1,16 @@
 // eslint-disable-next-line import/no-unresolved
 import { addTempWrapper } from '../../scripts/decorate.js';
 import { createTag, pickRandomFromArray } from '../../scripts/utils.js';
-// import { throttle } from '../../scripts/hofs.js';
 
 const DEFAULT_BACKGROUND_OPACITY = 1;
 
 export default function decorate($block) {
-  console.log('=== ENTERING decorate');
-
   addTempWrapper($block, 'quotes');
 
   const isSingularVariant = $block.classList.contains('singular');
 
-  console.log('=== $block, isSingularVariant', $block, isSingularVariant);
-
   if (isSingularVariant) {
     const $rows = [...$block.querySelectorAll(':scope>div')];
-
-    console.log('=== rows', $rows);
-
-    console.log('=== rows', $rows.map(e => e.children));
-
-    // const $newBlock = createTag('div', { class: 'new-block' });
 
     // The Desktop design has a different element layout from the mobile design, plus
     // the desktop design uses the background image twice, while the mobile design uses
@@ -29,9 +18,7 @@ export default function decorate($block) {
     const $quoteContainer = createTag('div', { class: 'quote-container' });
     const $desktopContainer = createTag('div', { class: 'desktop-container' });
     const $mobileContainer = createTag('div', { class: 'mobile-container' });
-
     const $desktopContainerBackground = createTag('div', { class: 'background' });
-
     const $mobileContainerBackground = createTag('div', { class: 'background' });
 
     $desktopContainer.append($desktopContainerBackground);
@@ -39,59 +26,11 @@ export default function decorate($block) {
     $quoteContainer.append($desktopContainer);
     $quoteContainer.append($mobileContainer);
 
-    console.log('=== $rows[$rows.length - 1]', $rows[$rows.length - 1]);
-
-    console.log('=== $rows[$rows.length - 1].children', $rows[$rows.length - 1].children);
-
-    console.log(
-      '=== $rows[$rows.length - 1].children',
-      $rows[$rows.length - 1].children[0].innerHTML
-    );
-    console.log(
-      '=== $rows[$rows.length - 1].children',
-      $rows[$rows.length - 1].children[1].innerHTML
-    );
-
-    console.log(
-      '=== $rows[$rows.length - 1].children[0].textContent',
-      JSON.stringify($rows[$rows.length - 1].children[0].textContent)
-    );
-    console.log(
-      '=== $rows[$rows.length - 1].children[0].innerText',
-      JSON.stringify($rows[$rows.length - 1].children[0].innerText)
-    );
-
-    // let $firstRow = $rows[0];
-    let $lastRow = $rows[$rows.length - 1];
-
-    console.log("=== $rows[0].children", $rows[0].children, $rows[0].children.length)
-
-    // return;
-    // let hasOpacity, opacitySpecified;
-    // if ($lastRow.children[0].textContent.trim().toLowerCase() === 'opacity') {
-    //   hasOpacity = true;
-    //   console.log(
-    //     '=== $lastRow.children[1] opacity',
-    //     $lastRow.children[1],
-    //     JSON.stringify($lastRow.children[1].innerText),
-    //     JSON.stringify($lastRow.children[1].textContent)
-    //   );
-    //   opacitySpecified = +$lastRow.children[1].textContent;
-    //   $rows.pop();
-    // } else {
-    //   hasOpacity = false;
-    // }
-
-    // console.log('=== hasOpacity, opacitySpecified', hasOpacity, opacitySpecified);
-
-
     let hasBackground, backgroundUrl;
     if ($rows[0].children.length === 1) {
       hasBackground = true;
       const $img = $rows[0].children[0].querySelector('img');
-      console.log('=== img', $img);
       backgroundUrl = $img.src;
-      console.log('=== backgroundUrl', backgroundUrl);
 
       const backgroundDesktopCSS = `no-repeat calc(-600px + 50%) 0 url("${backgroundUrl}"), no-repeat calc(600px + 50%) 0  url("${backgroundUrl}")`;
       $desktopContainerBackground.style.background = backgroundDesktopCSS;
@@ -104,54 +43,11 @@ export default function decorate($block) {
       hasBackground = false;
     }
 
-    // $lastRow = $rows[$rows.length - 1];
-
-    // let hasBackground, backgroundUrl;
-    // if ($lastRow.children[0].textContent.trim().toLowerCase() === 'background') {
-    //   hasBackground = true;
-    //   const $img = $lastRow.children[1].querySelector('img');
-    //   console.log('=== img', $img);
-    //   backgroundUrl = $img.src;
-    //   console.log('=== backgroundUrl', backgroundUrl);
-
-    //   $rows.pop();
-    //   // $quotes = $rows.slice(0, $rows.length - 1);
-
-    //   // console.log('=== $newBlock.parentElement', $newBlock.parentElement);
-    //   // console.log('=== $newBlock.parentNode', $newBlock.parentNode);
-
-    //   const backgroundDesktopCSS = `no-repeat calc(-600px + 50%) 0 url("${backgroundUrl}"), no-repeat calc(600px + 50%) 0  url("${backgroundUrl}")`;
-    //   $desktopContainerBackground.style.background = backgroundDesktopCSS;
-    //   // $desktopContainerBackground.style.opacity = opacitySpecified ?? DEFAULT_BACKGROUND_OPACITY;
-
-    //   const backgroundMobileCSS = `no-repeat 15% 12% url("${backgroundUrl}")`;
-    //   $mobileContainerBackground.style.background = backgroundMobileCSS;
-    //   // $mobileContainerBackground.style.opacity = opacitySpecified ?? DEFAULT_BACKGROUND_OPACITY;
-    // } else {
-    //   hasBackground = false;
-    //   // $quotes = $rows;
-    // }
-
     // at this point, $rows contains only quotes (no param)
     const $quoteSelected = pickRandomFromArray($rows);
-
-    console.log(
-      '=== $quoteSelected, hasBackground, backgroundUrl',
-      $quoteSelected,
-      hasBackground,
-      backgroundUrl
-    );
-
-    console.log('=== card', $quoteSelected, $quoteSelected.querySelector);
-
-    console.log('=== children', $quoteSelected.children[0]);
-
     const $picture = $quoteSelected.querySelector('picture');
-
-    console.log('=== $picture', $picture);
-
     const $quoteDesktop = createTag('div', { class: 'quote' });
-    // $desktopContainer.append($desktopContainerBackground);
+
     $desktopContainer.append($quoteDesktop);
 
     const $authorPhoto = createTag('div', { class: 'author-photo' });
@@ -167,16 +63,9 @@ export default function decorate($block) {
 
     const $review = $quoteSelected.children[0];
 
-    // $quoteTextComment.append(`“${$review.innerText.replace(/”$/, '').replace(/"$/, '')}”`);
     $quoteComment.append($review.textContent);
 
     const authorDescription = $quoteSelected.children[1].textContent;
-
-    // const authorDescription2 = $quoteSelected.children[1].innerText.trim().replace(/\n/, ',');
-    // .replace(/\s+$/, '');
-    // .replace("/\n[^\n]+$/", ",");
-
-    console.log('=== authorDescription', JSON.stringify(authorDescription));
 
     const $authorDescription = createTag('div', { class: 'author-description' });
     $quoteDetails.append($authorDescription);
@@ -200,24 +89,17 @@ export default function decorate($block) {
     });
     $quoteDetailsMobile.append($quoteAuthorPanelMobile);
 
-
     const $quoteAuthorPhotoMobile = createTag('div', {
       class: 'author-photo',
     });
     $quoteAuthorPanelMobile.append($quoteAuthorPhotoMobile);
 
     const $pictureCloned = $picture.cloneNode(true);
-    console.log('=== $pictureCloned', $pictureCloned);
 
     $quoteAuthorPhotoMobile.append($pictureCloned);
-
     $quoteAuthorPanelMobile.append(authorDescription);
 
-
-    // $quoteContainer.append($desktopContainer);
-
     $block.replaceChildren($quoteContainer);
-    console.log('=== FINAL $block', $block);
   } else {
     $block.querySelectorAll(':scope>div').forEach(($card) => {
       $card.classList.add('quote');
