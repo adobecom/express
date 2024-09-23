f3395450886ce6b38dbec12e9cb22f0949682d1b
Haris Bijli <all46558@adobe.com>
2024-09-23 13:24:04 -0700

    update urls

--- test/e2e/features/checkPages.feature
+++ test/e2e/features/checkPages.feature
@@ -1,13 +1,7 @@
 Feature: Check Important Express Pages
 
-@smoke-test
+@smoke-test @regression
   Scenario: Verify that all URLs return a valid status code
     Given a list of URLs
     When I check each URL
     Then there should be no 404 status codes
-
-  Examples:
-    | URL              |
-    | #                |
-    | templates/flyer  |
-    | create/menu      |
--- test/e2e/step-definitions/checkPages.steps.js
+++ test/e2e/step-definitions/checkPages.steps.js
@@ -1,5 +1,4 @@
 import { Given, When, Then } from '@cucumber/cucumber';
-import { expect } from "@playwright/test";
 
 const playwright = require('playwright');
 const YAML = require('yamljs');
@@ -16,23 +15,17 @@ Given('a list of URLs', () => {
 When('I check each URL', async () => {
   const browser = await playwright.chromium.launch();
   const context = await browser.newContext();
-  const page = await context.newPage();
 
   for (const url of urls) {
-    try {
-      const response = await page.goto(url);
-      const statusCode = response.status();
-      responseCodes.push({ url, statusCode });
-    } catch (error) {
-      responseCodes.push({ url, statusCode: 'Network error' });
-    }
+    const response = await context.request.get(url);
+    const statusCode = response.status();
+    responseCodes.push({ url, statusCode });
   }
   await browser.close();
 });
 
 Then('there should be no 404 status codes', () => {
   responseCodes.forEach(({ url, statusCode }) => {
-    console.log(`${url} returned status code: ${statusCode}`);
     assert(statusCode !== 404, `${url} returned a 404 status code`);
   });
 });
