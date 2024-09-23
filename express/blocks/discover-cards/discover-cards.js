74b2e9a5ce03b7e204701b4cbd08323ab2a16bbc
Jackson Sandland <all48775@adobe.com>
2024-09-23 12:34:01 -0700

    set up short and tall classes for card variants

--- express/blocks/discover-cards/discover-cards.js
+++ express/blocks/discover-cards/discover-cards.js
@@ -2,7 +2,7 @@ import { createTag } from '../../scripts/utils.js';
 import buildGallery from '../../features/gallery/gallery.js';
 
 export default async function decorate(block) {
-  const isBottomImageVariant = !block.classList.contains('bottom-image');
+  const isBottomImageVariant = block.classList.contains('bottom-image');
   const firstChild = block.querySelector(':scope > div:first-child');
 
   if (firstChild && firstChild.querySelector('h3')) {
@@ -19,6 +19,9 @@ export default async function decorate(block) {
   cards.forEach((card, index) => {
     if (index === 0 && firstChild) return;
     card.classList.add('card');
+    if (isBottomImageVariant) {
+      card.classList.add('short');
+    }
     cardsWrapper.appendChild(card);
 
     const cardDivs = [...card.children];
@@ -31,12 +34,34 @@ export default async function decorate(block) {
     cardDivs.forEach((element) => {
       const textHeader = element.querySelector('h4');
       const textBody = element.querySelector('p');
-      if (textHeader && textBody) {
+      if (textHeader && textBody && !isBottomImageVariant) {
         textHeader.style.textAlign = 'left';
         textBody.style.marginTop = '5px';
         textBody.style.marginBottom = '5px';
         element.style.textAlign = 'left';
       }
+
+      if (textHeader && textBody && isBottomImageVariant) {
+        textHeader.style.fontWeight = 600;
+        textHeader.style.fontSize = '18px';
+        textHeader.style.textAlign = 'left';
+        element.style.textAlign = 'left';
+        element.style.display = 'contents';
+        textBody.style.marginTop = '5px';
+        textBody.style.marginBottom = '5px';
+        textBody.style.fontSize = '14px';
+      }
+
+      const image = element.querySelector('picture img');
+      if (image && isBottomImageVariant) {
+        image.classList.add('tall');
+        image.classList.remove('short');
+      }
+      if (image && !isBottomImageVariant) {
+        image.classList.add('short');
+        image.classList.remove('tall');
+      }
+
       if (element.tagName === 'H2') {
         element.classList.add('card-title');
       } else if (element.querySelector('a.button')) {
