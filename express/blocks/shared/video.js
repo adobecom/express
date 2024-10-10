c5e70125b9a00b7297676f93e15e8c63955a51cd
echen-adobe <all42238@adobe.com>
2024-10-10 14:10:54 -0700

    Update video.js

--- express/blocks/shared/video.js
+++ express/blocks/shared/video.js
@@ -308,6 +308,7 @@ export function displayVideoModal(url = [], title, push) {
   }
 }
 
+
 export function playPreloadedVideo(videoID) {
   const videoOverlays = document.querySelectorAll(".video-overlay-preloaded")
   for (let vo of videoOverlays) {
@@ -384,14 +385,14 @@ export function preloadVideoModal(url = [], title, push, videoID) {
   buildVideoElement($video, vidUrls, vidType, title, ts);
 }
 
-function buildVideoElement($video, vidUrls = [], playerType, title, ts) {
+function buildVideoElement($element, vidUrls = [], playerType, title, ts) {
   const [primaryUrl] = vidUrls;
   if (!primaryUrl) return;
   if (playerType === 'html5') {
     const sources = vidUrls.map((src) => `<source src="${src}" type="${getMimeType(src)}"></source>`).join('');
     const videoHTML = `<video controls playsinline>${sources}</video>`;
-    $video.innerHTML = videoHTML;
-    const $video = $video.querySelector('video');
+    $element.innerHTML = videoHTML;
+    const $video = $element.querySelector('video');
     $video.addEventListener('loadeddata', async () => {
       if (ts) {
         $video.currentTime = ts;
@@ -403,13 +404,13 @@ function buildVideoElement($video, vidUrls = [], playerType, title, ts) {
       const videoAnalytic = await getVideoAnalytic($video);
       const promoName = videoPromos[primaryUrl];
       if (typeof promoName === 'string') {
-        $video.insertAdjacentHTML('beforeend', `<div class="promotion block" data-block-name="promotion">${promoName}</div>`);
-        const $promo = $video.querySelector('.promotion');
+        $element.insertAdjacentHTML('beforeend', `<div class="promotion block" data-block-name="promotion">${promoName}</div>`);
+        const $promo = $element.querySelector('.promotion');
         await loadBlock($promo, true);
         $promo.querySelector(':scope a.button').className = 'button accent';
         const $PromoClose = $promo.appendChild(createTag('div', { class: 'close' }));
         $PromoClose.addEventListener('click', () => {
-          closeVideo($video);
+          closeVideo($element);
         });
         window.videoPromotions[primaryUrl] = $promo;
       }
@@ -442,7 +443,7 @@ function buildVideoElement($video, vidUrls = [], playerType, title, ts) {
       scrolling: 'no',
     });
 
-    $video.replaceChildren($iframe);
+    $element.replaceChildren($iframe);
   } else {
     // iframe 3rd party player
     const $iframe = createTag('iframe', {
@@ -453,16 +454,18 @@ function buildVideoElement($video, vidUrls = [], playerType, title, ts) {
       allowfullscreen: '',
     });
 
-    $video.replaceChildren($iframe);
+    $element.replaceChildren($iframe);
   }
 
-  const $videoClose = $video.appendChild(createTag('div', { class: 'close' }));
+
+
+  const $videoClose = $element.appendChild(createTag('div', { class: 'close' }));
   $videoClose.addEventListener('click', async () => {
-    closeVideo($video);
+    closeVideo($element);
   });
 
-  $video.classList.add(playerType);
-  $video.querySelector('video').pause()
+  $element.classList.add(playerType);
+  $element.querySelector('video').pause()
 }
 
 async function closeVideo($video) {
