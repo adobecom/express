b4c4e5c5397f19bfdf717a68fa8f04eade593c5c
Jingle Huang <jingleh321@gmail.com>
2024-10-14 10:44:15 -0700

    fix mobile logic

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -82,8 +82,7 @@ const eagerLoad = (img) => {
   const { userAgent } = navigator;
   document.body.dataset.device = userAgent.includes('Mobile') ? 'mobile' : 'desktop';
   const fqaMeta = createTag('meta', { content: 'on' });
-  if (document.body.dataset.device === 'mobile'
-    && !(/Safari/.test(userAgent) || /Chrome|CriOS|FxiOS|Edg|OPR|Opera|OPiOS|Vivaldi|YaBrowser|Avast|VivoBrowser|GSA/.test(userAgent))) {
+  if (/Safari/.test(userAgent) && !/Chrome|CriOS|FxiOS|Edg|OPR|Opera|OPiOS|Vivaldi|YaBrowser|Avast|VivoBrowser|GSA/.test(userAgent)) {
     fqaMeta.setAttribute('name', 'fqa-off');
   } else {
     fqaMeta.setAttribute('name', 'fqa-on');
