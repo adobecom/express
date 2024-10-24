9986aefcdaf9e27a9e75d13b2cf8393daad042d0
skholkhojaev <skholkhojaev@adobe.com>
2024-10-24 11:38:39 +0200

    Re-run Adobe CLA

--- test/unit/blocks/table-of-contents/table-of-contents.test.js
+++ test/unit/blocks/table-of-contents/table-of-contents.test.js
@@ -23,6 +23,7 @@ describe('table-of-contents', () => {
     block.dataset.config = JSON.stringify(config);
 
     decorate(block, 'table-of-contents', document);
+    
 
     const tocEntries = document.querySelectorAll('.toc-entry');
     tocEntries.forEach((entry) => {
