22d790c324a88369100ba89b68e7fc67cf333fb4
echen-adobe <all42238@adobe.com>
2024-11-05 11:34:15 -0800

    CSS fixes

--- express/blocks/mobile-gating-button/mobile-gating-button.js
+++ express/blocks/mobile-gating-button/mobile-gating-button.js
@@ -1,6 +1,6 @@
 import { addTempWrapper } from '../../scripts/decorate.js';
 import {
-  createTag, getMetadata, getMobileOperatingSystem,
+  createTag, getIconElement, getMetadata, getMobileOperatingSystem,
 } from '../../scripts/utils.js';
 
 import {
@@ -20,12 +20,19 @@ function buildAction(entry, buttonType) {
 
 function buildMobileGating(block, data) {
   block.children[0].remove();
+  const headerRow = createTag('div', { class : 'mobile-gating-header-row'})
   const header = createTag('div', {
     class:
       'mobile-gating-header',
   });
   header.textContent = data.mainCta.text;
-  block.append(header, buildAction(data.tools[0], 'accent'), buildAction(data.tools[1], 'outline'));
+  const exit = createTag('div', { class : 'mobile-gating-exit'})
+  exit.addEventListener('click', () => {
+    block.parentElement.classList.add('floating-button--hidden')
+  })
+  exit.append(getIconElement('close-button-x', 14))
+  headerRow.append(header, exit)
+  block.append(headerRow, buildAction(data.tools[0], 'accent'), buildAction(data.tools[1], 'outline'));
 }
 
 export function createMultiFunctionButton(block, data, audience) {
