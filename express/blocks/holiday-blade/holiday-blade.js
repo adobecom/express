50ac98ad5cbef6595716b1866ed7238353be4cfe
echen-adobe <all42238@adobe.com>
2024-11-12 11:55:49 -0800

    Update holiday-blade.js

--- express/blocks/holiday-blade/holiday-blade.js
+++ express/blocks/holiday-blade/holiday-blade.js
@@ -44,7 +44,7 @@ async function decorateHoliday(block, props) {
     block.classList.add('animated');
     block.append(animation);
 
-    fetchAndRenderTemplates(props).then((res) => {})
+    const a = await fetchAndRenderTemplates(props)
         // const { templates, fallbackMsg } = res
         // for (let i = 1; i < 4; i++) {
         //     rows[i].innerHTML = ''
