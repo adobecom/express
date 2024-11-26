8774dc6c39bdd72c64abc1517d268b3e750486e1
Jackson Sandland <all48775@adobe.com>
2024-11-26 11:44:07 -0700

    use template-x-carousel variant to activate template-x-carousel

--- express/blocks/shared/carousel-wrapper.js
+++ express/blocks/shared/carousel-wrapper.js
@@ -1,9 +1,8 @@
 import buildCarousel from './carousel.js';
 import buildTemplateXCarousel from './template-x-carousel.js';
 
-const useTemplateXCarousel = true;
-
 function loadCarousel(selector, parent, options) {
+  const useTemplateXCarousel = parent.closest('.template-x-carousel');
   const carouselLoader = useTemplateXCarousel ? buildTemplateXCarousel : buildCarousel;
   return carouselLoader(selector, parent, options);
 }
