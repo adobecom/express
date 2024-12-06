b3ea1b28bdc76625cfdbe0debcd09dfd76bb63a1
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2024-12-06 12:12:23 -0800

    Sync Stage With Main (#1308)
    * reverted single video variant
    
    * Update columns.css
    
    * Update columns.css
    
    * Update columns.css

--- express/blocks/columns/columns.js
+++ express/blocks/columns/columns.js
@@ -471,8 +471,4 @@ export default async function decorate(block) {
       });
     });
   }
-
-  if (Array.from(block.querySelectorAll('.column-picture')).length === 1) {
-    block.classList.add('single-video');
-  }
 }
