2754ea0ec27eda3bb2c8e1faae19a14e521828fb
Jackson Sandland <all48775@adobe.com>
2024-12-11 15:28:02 -0700

    stopPropagation test for iOS

--- express/blocks/shared/basic-carousel.js
+++ express/blocks/shared/basic-carousel.js
@@ -152,7 +152,7 @@ function initializeCarousel(selector, parent) {
 
   platform.addEventListener('touchend', (e) => {
     e.preventDefault(); // Prevents default behavior
-    // e.stopPropagation(); // Stops bubbling if needed
+    e.stopPropagation(); // Stops bubbling if needed
     const swipeDistance = touchEndX - touchStartX;
 
     if (Math.abs(swipeDistance) > 50) {
